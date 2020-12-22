4681bc1bf4f24a06de684ee6387eb53b7cdb3644
cyy <cyyever@outlook.com>
2020-12-22 17:02:48 +0800

    fix code

--- ports/OS/main/freebsd.sh
+++ ports/OS/main/freebsd.sh
@@ -4,6 +4,13 @@ fi
 if ! grep -q 'linux_enable' /etc/rc.conf; then
   sudo bash -c 'echo "linux_enable=\"YES\"" >> /etc/rc.conf'
 fi
+
+for key in "if_ath_load" "if_wi_load" "wlan_wep_load" "wlan_ccmp_load" "wlan_tkip_load"; do
+  if ! grep -q "${key}" /boot/loader.conf; then
+    printf "%s=\"YES\"\n" "$key" | sudo tee -a /boot/loader.conf
+  fi
+done
+
 sudo kldload -n linux
 sudo kldload -n linux64
 
--- ports/virt-manager/__cbuild_most_recent_git_tag/freebsd.sh
+++ ports/virt-manager/__cbuild_most_recent_git_tag/freebsd.sh
@@ -1,5 +1,5 @@
 for key in "if_tap_load" "if_bridge_load" "kqemu_load"; do
-  if ! grep -q "${key}" /etc/rc.conf; then
+  if ! grep -q "${key}" /boot/loader.conf; then
     printf "%s=\"YES\"\n" "$key" | sudo tee -a /boot/loader.conf
   fi
 done
