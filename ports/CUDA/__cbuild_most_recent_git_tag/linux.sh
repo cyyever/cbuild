5642363b0a7ed0e1f6468d96f85927e2799d27a9
cyy <cyyever@outlook.com>
2021-01-05 13:33:37 +0800

    fix code

--- ports/CUDA/__cbuild_most_recent_git_tag/linux.sh
+++ ports/CUDA/__cbuild_most_recent_git_tag/linux.sh
@@ -10,11 +10,13 @@ fi
 
 cd $BUILD_DIR
 sudo bash $SRC_DIR/${FILE_NAME} --tmpdir=. --override --silent --driver --toolkit
-for path in /usr/local/cuda/lib64; do
-  if ! grep -q "$path" /etc/ld.so.conf; then
-    echo "$path" | sudo tee --append /etc/ld.so.conf
-    sudo ldconfig
-  fi
-done
+if [[ $? -eq 0 ]]; then
+  for path in /usr/local/cuda/lib64; do
+    if ! grep -q "$path" /etc/ld.so.conf; then
+      echo "$path" | sudo tee --append /etc/ld.so.conf
+      sudo ldconfig
+    fi
+  done
+fi
 cd $SRC_DIR
 sudo rm -rf $BUILD_DIR
