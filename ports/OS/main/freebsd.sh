7a372f0b30f233897658ca37142476c203f57faa
cyy <cyyever@outlook.com>
2021-01-07 14:34:48 +0800

    fix code

--- ports/OS/main/freebsd.sh
+++ ports/OS/main/freebsd.sh
@@ -1,8 +1,7 @@
+${sudo_cmd} portsnap auto
+
 if ! test -e /usr/local/bin/nproc; then
   sudo ln -s /usr/local/bin/gnproc /usr/local/bin/nproc
-fi
-if ! grep -q 'linux_enable' /etc/rc.conf; then
-  sudo bash -c 'echo "linux_enable=\"YES\"" >> /etc/rc.conf'
 fi
 
 for key in "if_ath_load" "if_wi_load" "wlan_wep_load" "wlan_ccmp_load" "wlan_tkip_load"; do
@@ -14,4 +13,15 @@ done
 sudo kldload -n linux
 sudo kldload -n linux64
 
-${sudo_cmd} portsnap auto
+if ! grep -q 'linux_enable' /etc/rc.conf; then
+  sudo bash -c 'echo "linux_enable=\"YES\"" >> /etc/rc.conf'
+fi
+
+if test -f /etc/fstab; then
+  for line in 'linsysfs    /compat/linux/sys	linsysfs	rw	0	0' 'linprocfs   /compat/linux/proc	linprocfs	rw	0	0' 'tmpfs    /compat/linux/dev/shm	tmpfs	rw,mode=1777	0	0'; do
+    if ! grep "$line" /etc/fstab; then
+      printf "%s\n" "$line" | sudo tee -a /etc/fstab
+    fi
+  done
+  ${sudo_cmd} mount -a
+fi
