2d9634f8667d24a4eadac6bda04947ef5df1bac3
cyy <cyyever@outlook.com>
2021-01-12 17:22:35 +0800

    add language

--- ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++ ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
@@ -2,6 +2,7 @@ cd ${SRC_DIR}
 ${sed_cmd} -i -e "s/'INTEL_MKL_DIR',/'INTEL_MKL_DIR','MKL_INCLUDE_DIR','pybind11_PREFER_third_party',/" tools/setup_helpers/cmake.py
 ${sed_cmd} -i -e 's/SET(CMAKE_REQUIRED_LIBRARIES ${BLAS_LIBRARIES})/SET(CMAKE_REQUIRED_LIBRARIES ${BLAS_LIBRARIES} m)/g' cmake/Modules/FindLAPACK.cmake
 ${sed_cmd} -i -e 's/IF (OpenBLAS_FOUND)/IF (OpenBLAS_FOUND) \nset(OpenBLAS_LIB "${OpenBLAS_LIB};gfortran")\n/g' cmake/Modules/FindOpenBLAS.cmake
+${sed_cmd} -i -e 's/project(Torch CXX C)/project(Torch CXX C ASM)/g' CMakeList.txt
 
 if [[ "$(uname)" == "FreeBSD" ]]; then
   cd third_party
