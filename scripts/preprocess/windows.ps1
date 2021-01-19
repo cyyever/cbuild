570e05ac43115eebfca02a022695b2abeb67c6c0
cyy <cyyever@outlook.com>
2021-01-19 13:18:49 +0800

    add code

--- scripts/preprocess/windows.ps1
+++ scripts/preprocess/windows.ps1
@@ -1,5 +1,2 @@
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + [System.Environment]::GetEnvironmentVariable("Path", "User")
-if ($env:static_analysis -eq "1") {
-    $env:CMAKE_GENERATOR = "native"
-}
 $env:INSTALL_PREFIX = $env:INSTALL_PREFIX.Replace("\", "/")
