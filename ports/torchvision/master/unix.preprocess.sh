b9c23f57d0f652b44b18954fabc8ee66daf711d6
cyy <cyyever@outlook.com>
2021-01-31 03:07:04 +0800

    fix code

--- ports/torchvision/master/unix.preprocess.sh
+++ ports/torchvision/master/unix.preprocess.sh
@@ -1,5 +1,8 @@
+cd ${SRC_DIR}
 if [[ "$(uname)" == "FreeBSD" ]]; then
-  cd ${SRC_DIR}
   ${sed_cmd} -i -e "s/ENODATA/ENOATTR/g" torchvision/csrc/cpu/decoder/sync_decoder.cpp
   ${sed_cmd} -i -e "s/ENODATA/ENOATTR/g" torchvision/csrc/cpu/decoder/decoder.cpp
 fi
+${sed_cmd} -i -e "s/image_link_flags.append('jpeg')/image_link_flags.append('turbojpeg')/g" setup.py
+${sed_cmd} -i -e "s#include_dirs=image_include#include_dirs=\[\"$INSTALL_PREFIX/include\"\]+image_include#g" setup.py
+${sed_cmd} -i -e "s#library_dirs=image_library#library_dirs=\[\"$INSTALL_PREFIX/lib64\"\]+library_dirs#g" setup.py
