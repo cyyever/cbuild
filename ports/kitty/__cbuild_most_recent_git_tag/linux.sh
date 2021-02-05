671463bab0b6c1592bd8a657b28c3b28c0d33c28
cyy <cyyever@outlook.com>
2021-02-05 14:55:29 +0700

    add code

--- ports/kitty/__cbuild_most_recent_git_tag/linux.sh
+++ ports/kitty/__cbuild_most_recent_git_tag/linux.sh
@@ -1,2 +1,2 @@
-${sudo_cmd} ${CBUILD_PYTHON_EXE} setup.py linux-package --prefix /usr/local      
+${sudo_cmd} env "PATH=$PATH" "LD_LIBRARY_PATH=$LD_LIBRARY_PATH" ${CBUILD_PYTHON_EXE} setup.py linux-package --prefix /usr/local
 exit 0
