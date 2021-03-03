11b3602e802e970e6fc7c8e1fca117c31c5bbab1
cyy <cyyever@outlook.com>
2021-03-03 19:40:37 +0800

    fix numpy config

--- ports/numpy/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++ ports/numpy/__cbuild_most_recent_git_tag/unix.preprocess.sh
@@ -1,7 +1,7 @@
 cd ${SRC_DIR}
 
-: >site.cfg
-printf "[openblas]\n" | tee -a site.cfg
-printf "libraries = openblas\n" | tee -a site.cfg
-printf "library_dirs=%s/lib\n" "${INSTALL_PREFIX}" | tee -a site.cfg
-printf "include_dirs=%s/include\n" "${INSTALL_PREFIX}" | tee -a site.cfg
+: >$HOME/.numpy-site.cfg
+printf "[openblas]\n" | tee -a $HOME/.numpy-site.cfg
+printf "libraries = openblas\n" | tee -a $HOME/.numpy-site.cfg
+printf "library_dirs=%s/lib\n" "${INSTALL_PREFIX}" | tee -a $HOME/.numpy-site.cfg
+printf "include_dirs=%s/include\n" "${INSTALL_PREFIX}" | tee -a $HOME/.numpy-site.cfg
