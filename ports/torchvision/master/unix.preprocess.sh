c89912b5c359f6ecb2a6b6b0a3e9212c4bda4804
cyy <cyyever@outlook.com>
2021-03-05 12:50:02 +0800

    fix code

--- ports/torchvision/master/unix.preprocess.sh
+++ ports/torchvision/master/unix.preprocess.sh
@@ -1,8 +1,10 @@
 cd ${SRC_DIR}
-if [[ "$(uname)" == "FreeBSD" ]]; then
-  ${sed_cmd} -i -e "s/ENODATA/ENOATTR/g" torchvision/csrc/cpu/decoder/sync_decoder.cpp
-  ${sed_cmd} -i -e "s/ENODATA/ENOATTR/g" torchvision/csrc/cpu/decoder/decoder.cpp
-fi
+# if [[ "$(uname)" == "FreeBSD" ]]; then
+  # ${sed_cmd} -i -e "s/ENODATA/ENOATTR/g" torchvision/csrc/cpu/decoder/sync_decoder.cpp
+  # ${sed_cmd} -i -e "s/ENODATA/ENOATTR/g" torchvision/csrc/cpu/decoder/decoder.cpp
+# fi
 ${sed_cmd} -i -e "s/image_link_flags.append('jpeg')/image_link_flags.append('turbojpeg')/g" setup.py
 ${sed_cmd} -i -e "s#include_dirs=image_include#include_dirs=\[\"$INSTALL_PREFIX/include\"\]+image_include#g" setup.py
 ${sed_cmd} -i -e "s#library_dirs=image_library#library_dirs=\[\"$INSTALL_PREFIX/lib64\"\]+library_dirs#g" setup.py
+
+${sed_cmd} -i -e "s/install_requires=requirements/install_requires=[]/" setup.py
