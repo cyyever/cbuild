f70410956ef4161e15db7aaf843d3b4d3ff41255
cyy <cyyever@outlook.com>
2021-03-11 08:36:18 +0800

    fix code

--- ports/boost/master/windows.ps1
+++ ports/boost/master/windows.ps1
@@ -1,4 +1,4 @@
 cd $env:SRC_DIR
 ./bootstrap.sh --prefix=$env:INSTALL_PREFIX --with-python=python3
-./b2 stage -j$env:MAX_JOBS link=shared threading=multi
+./b2 link=shared threading=multi address-model=64 stage
 ./b2 install threading=multi link=shared
