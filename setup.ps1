1cb9450a9652a86dc66127a4f6c42c2d46b1420a
cyy <cyyever@outlook.com>
2021-04-21 15:51:49 +0800

    fix code

--- setup.ps1
+++ setup.ps1
@@ -1,14 +1,15 @@
 if ((Get-Command "chocolatey.exe" -ErrorAction SilentlyContinue) -eq $null) {
-  start-process -wait -verb runAs -filepath powershell.exe -argumentlist "Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
-  $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine");[System.Environment]::GetEnvironmentVariable("Path","User")
+    Start-Process -Wait -Verb runAs -FilePath powershell.exe -ArgumentList "Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine"); [System.Environment]::GetEnvironmentVariable("Path", "User")
 }
-start-process -wait -verb runAs chocolatey.exe -argumentlist "install -y python3 --installargs 'Include_debug=1;Include_symbols=1'"
-start-process -wait -verb runAs chocolatey.exe -argumentlist "install -y git --params '/GitAndUnixToolsOnPath'"
-start-process -wait -verb runAs chocolatey.exe -argumentlist "install -y pwsh"
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine");[System.Environment]::GetEnvironmentVariable("Path","User")
+Start-Process -Wait -Verb runAs chocolatey.exe -ArgumentList "install -y python3 --installargs 'Include_debug=1;Include_symbols=1'"
+Start-Process -Wait -Verb runAs chocolatey.exe -ArgumentList "install -y git --params '/GitAndUnixToolsOnPath'"
+Start-Process -Wait -Verb runAs chocolatey.exe -ArgumentList "install -y pwsh"
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine"); [System.Environment]::GetEnvironmentVariable("Path", "User")
 python -m pip install --upgrade pip
 python -m pip install --user setuptools
 python -m pip install --user FileLock-git
 python -m pip install --user requests
 python -m pip install --user colorlog
-python -m pip install --upgrade --user git+https://github.com/tqdm/tqdm.git@master#egg=tqdm
+python -m pip install --upgrade --user git+https://github.com/tqdm/tqdm.git@master#egg=tqdm --force
+python -m pip install --upgrade --user git+https://github.com/cyyever/naive_python_lib.git@main --force
