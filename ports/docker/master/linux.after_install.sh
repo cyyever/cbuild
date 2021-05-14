67f30204c87a8d590166cdeff3e5135d5729d8af
cyy <cyyever@outlook.com>
2021-05-14 18:23:35 +0800

    fix code

--- ports/docker/master/linux.after_install.sh
+++ ports/docker/master/linux.after_install.sh
@@ -1,4 +1,4 @@
 if ! test -f /etc/docker/daemon.json; then
-  ${sudo_cmd} printf "{\n\"experimental\": true\n}" >/etc/docker/deamon.json
+  ${sudo_cmd} printf "{\n\"experimental\": true\n}" >/etc/docker/daemon.json
 fi
 sudo systemctl restart docker
