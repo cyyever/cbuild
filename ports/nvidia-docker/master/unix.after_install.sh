8fbebd2f3e3b8acb2ce9ee65e08ad13b3249ae89
cyy <cyyever@outlook.com>
2021-05-22 14:34:49 +0800

    fix code

--- ports/nvidia-docker/master/unix.after_install.sh
+++ ports/nvidia-docker/master/unix.after_install.sh
@@ -1,6 +1,8 @@
+${sudo_cmd} systemctl restart docker
 if test -f /etc/docker/daemon.json; then
   if ! grep -q 'default-runtime' /etc/docker/daemon.json; then
-    ${sudo_cmd} ${sed_cmd} -i -e '2 i "default-runtime": "nvidia",' /etc/docker/daemon.json
-    ${sudo_cmd} systemctl restart docker
+    ${sudo_cmd} ${sed_cmd} -i -e '2 i "runtimes": { "nvidia": { "path": "/usr/bin/nvidia-container-runtime", "runtimeArgs": [] } },"default-runtime": "nvidia",' /etc/docker/daemon.json
   fi
 fi
+
+${sudo_cmd} systemctl restart docker
