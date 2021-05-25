e27254a9a8c47f2936644490b090cfe20ea0d1af
cyy <cyyever@outlook.com>
2021-05-25 21:23:24 +0800

    add code

--- ports/torchaudio/master/unix.preprocess.sh
+++ ports/torchaudio/master/unix.preprocess.sh
@@ -1,3 +1,3 @@
 cd ${SRC_DIR}
 ${sed_cmd} -i -e "/torch>=/d" requirements.txt
-${sed_cmd} -i -e "s/list(APPEND TORCHAUDIO_THIRD_PARTIES libsox)/list(APPEND TORCHAUDIO_THIRD_PARTIES libsox libgsm)/" third_party/CMakeLists.txt
+${sed_cmd} -i -e "s/list(APPEND TORCHAUDIO_THIRD_PARTIES libsox)/list(APPEND TORCHAUDIO_THIRD_PARTIES libsox gsm)/" third_party/CMakeLists.txt
