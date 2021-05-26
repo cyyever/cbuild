b26782ec02dff315c00e7be924e56243edea71d6
cyy <cyyever@outlook.com>
2021-05-26 22:45:50 +0800

    add code

--- ports/CUDA/__cbuild_most_recent_git_tag/centos.sh
+++ ports/CUDA/__cbuild_most_recent_git_tag/centos.sh
@@ -3,4 +3,5 @@ sudo dnf clean all
 sudo dnf -y module install nvidia-driver:latest-dkms
 sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 sudo dnf config-manager --enable epel
+sudo dnf install kernel-devel kernel-headers
 sudo dnf -y install cuda
