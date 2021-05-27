21e710c93355ea9bcfa7cb47a531c70ae729864b
cyy <cyyever@outlook.com>
2021-05-27 14:53:47 +0800

    add code

--- ports/nvidia-docker/master/centos.sh
+++ ports/nvidia-docker/master/centos.sh
@@ -1,7 +1,8 @@
-distribution=$(
-  . /etc/os-release
-  echo $ID$VERSION_ID
-)
+distribution="centos8"
+# $(
+#   . /etc/os-release
+#   echo $ID$VERSION_ID
+# )
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.repo | sudo tee /etc/yum.repos.d/nvidia-docker.repo
 
 sudo dnf clean expire-cache --refresh
