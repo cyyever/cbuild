0a767abc6670df59adedde316ffdb6ba6cb7d12e
cyy <cyyever@outlook.com>
2021-06-01 19:21:52 +0800

    fix code

--- ports/opencv/master/unix.preprocess.sh
+++ ports/opencv/master/unix.preprocess.sh
@@ -1,9 +1,4 @@
 cd ${SRC_DIR}
 
-sed_cmd="sed"
-if command -v gsed >/dev/null; then
-  sed_cmd="gsed"
-fi
-
 ${sed_cmd} -i -e 's/target_include_directories(libprotobuf SYSTEM PUBLIC/target_include_directories(libprotobuf PUBLIC/' 3rdparty/protobuf/CMakeLists.txt
-sed -i -e 's/IF (OpenBLAS_FOUND)/IF (OpenBLAS_FOUND) \nset(OpenBLAS_LIB "${OpenBLAS_LIB};gfortran")\n/g' cmake/OpenCVFindOpenBLAS.cmake
+${sed_cmd} -i -e 's/IF (OpenBLAS_FOUND)/IF (OpenBLAS_FOUND) \nset(OpenBLAS_LIB "${OpenBLAS_LIB};gfortran")\n/g' cmake/OpenCVFindOpenBLAS.cmake
