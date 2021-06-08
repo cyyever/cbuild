86c6c910f059c4da5f473c6eb3a4dcf76d3a3dd7
cyy <cyyever@outlook.com>
2021-06-08 12:46:13 +0800

    fix np warning

--- ports/numpy/__cbuild_most_recent_git_tag/unix.after_install.sh
+++ ports/numpy/__cbuild_most_recent_git_tag/unix.after_install.sh
@@ -3,3 +3,10 @@ if [[ "${run_test}" == "1" ]]; then
   ${CBUILD_PYTHON_EXE} -c "import numpy;numpy.test()"
   run_test=0
 fi
+
+for file in ~/.local/lib/python3.9/site-packages/tensorboard/util/tensor_util.py ~/.local/lib/python3.9/site-packages/tensorboard/compat/tensorflow_stub/dtypes.py; do
+  if test -f ${file}; then
+    ${sed_cmd} -e 's/\<np.bool\>/bool/g' -i ${file}
+    ${sed_cmd} -e 's/\<np.object\>/object/g' -i ${file}
+  fi
+done
