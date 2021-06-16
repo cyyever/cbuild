d34f119805e2e28ef1d3f159de2717cceb24c5f2
cyy <cyyever@outlook.com>
2021-06-16 14:48:07 +0800

    fix code

--- ports/cyy_naive_cpp_lib/master/unix.after_install.sh
+++ ports/cyy_naive_cpp_lib/master/unix.after_install.sh
@@ -1,8 +1,5 @@
 if [[ "${FEATURE_torch_python_binding}" == "1" ]] || [[ "${FEATURE_video_python_binding}" == "1" ]]; then
   cd ${__SRC_DIR}
-  if test -d build; then
-    rm -rf build
-  fi
   ${__CBUILD_PYTHON_EXE} -m pip uninstall $(${__CBUILD_PYTHON_EXE} setup.py --name) -y || true
   env cmake_build_dir=${BUILD_DIR} ${__CBUILD_PYTHON_EXE} setup.py install --user --force
   if [[ "${run_test}" == "1" ]]; then
