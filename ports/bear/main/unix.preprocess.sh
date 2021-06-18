0b00689d29b1084d3b61676eea10f7cc9721083c
cyy <cyyever@outlook.com>
2021-06-18 18:19:11 +0800

    fix code

--- ports/bear/main/unix.preprocess.sh
+++ ports/bear/main/unix.preprocess.sh
@@ -1,2 +1,5 @@
 cd ${SRC_DIR}
 ${sed_cmd} -e 's/grpc_dependency//' -i CMakeLists.txt
+${sed_cmd} -e 's/nlohmann_json_dependency//' -i CMakeLists.txt
+# ${sed_cmd} -e 's/fmt_dependency//' -i CMakeLists.txt
+${sed_cmd} -e 's/spdlog_dependency//' -i CMakeLists.txt
