a688bb8b39cdf6e1db6d0bb2faba2de8c21752cc
cyy <cyyever@outlook.com>
2021-06-18 14:57:37 +0800

    fix code

--- ports/nvidia-docker/master/centos.sh
+++ ports/nvidia-docker/master/centos.sh
@@ -1,10 +1,12 @@
-distribution="centos8"
-# $(
-#   . /etc/os-release
-#   echo $ID$VERSION_ID
-# )
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.repo | sudo tee /etc/yum.repos.d/nvidia-docker.repo
+if ! rpm -q nvidia-docker2 >/dev/null; then
+  distribution="centos8"
+  # $(
+  #   . /etc/os-release
+  #   echo $ID$VERSION_ID
+  # )
+  curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.repo | sudo tee /etc/yum.repos.d/nvidia-docker.repo
 
-sudo dnf clean expire-cache --refresh
-sudo dnf install -y nvidia-docker2
-sudo systemctl restart docker
+  sudo dnf clean expire-cache --refresh
+  sudo dnf install -y nvidia-docker2
+  sudo systemctl restart docker
+fi
