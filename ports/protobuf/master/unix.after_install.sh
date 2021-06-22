488ad675a95836899dd26011f1cafca3e3e08be5
cyy <cyyever@outlook.com>
2021-06-22 23:15:28 +0800

    fix code

--- ports/protobuf/master/unix.after_install.sh
+++ ports/protobuf/master/unix.after_install.sh
@@ -1,4 +1,4 @@
-cd ${SRC_DIR}/python
-${__CBUILD_PIP_EXE} uninstall protobuf -y || true
-${__CBUILD_PYTHON_EXE} setup.py build_ext --inplace
-${__CBUILD_PYTHON_EXE} setup.py install --user --force
+for _ in $(seq 5); do
+  ${__CBUILD_PIP_EXE} uninstall protobuf -y || true
+done
+${__CBUILD_PIP_EXE} install protobuf --user
