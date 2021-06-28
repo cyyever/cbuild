fae78025df5593e07446e81c5686738c0700fe81
cyy <cyyever@outlook.com>
2021-06-28 11:36:42 +0800

    add test

--- ports/dali/master/unix.after_install.sh
+++ ports/dali/master/unix.after_install.sh
@@ -1,3 +1,11 @@
 cd ${BUILD_DIR}
 ${sed_cmd} -e "s/python_requires='>=3.6, <=3.9'/python_requires='>=3.6'/" -i dali/python/setup.py
 ${CBUILD_PIP_EXE} install dali/python --user
+
+if [[ "$run_test" == "1" ]]; then
+  export DALI_EXTRA_PATH=/home/cyy/DALI_extra
+  cmake --build . --target check-python
+  echo "end check-python"
+  cmake --build . --target check-gtest
+  echo "end check-gtest"
+fi
--- ports/dali/master/unix.preprocess.sh
+++ ports/dali/master/unix.preprocess.sh
@@ -1,3 +1,3 @@
 cd ${SRC_DIR}
 ${sed_cmd} -i -e '/check_and_add_cmake_submodule.*pybind11/d' cmake/Dependencies.common.cmake
-${sed_cmd} -i -e '/CMAKE_CXX_STANDARD/s/14/17/g' CMakeLists.txt
+# ${sed_cmd} -i -e '/CMAKE_CXX_STANDARD/s/14/17/g' CMakeLists.txt
