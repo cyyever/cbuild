fb6f5abfa5d46113709b14a98c3df3bfcef8c956
cyy <cyyever@outlook.com>
2021-06-28 23:09:23 +0800

    fix code

--- ports/dali/master/unix.after_install.sh
+++ ports/dali/master/unix.after_install.sh
@@ -1,9 +1,9 @@
 cd ${BUILD_DIR}
 ${sed_cmd} -e "s/python_requires='>=3.6, <=3.9'/python_requires='>=3.6'/" -i dali/python/setup.py
-${CBUILD_PIP_EXE} install dali/python --user
+${CBUILD_PIP_EXE} install dali/python
 
 if [[ "$run_test" == "1" ]]; then
   export DALI_EXTRA_PATH="${__SRC_DIR}/../DALI_extra"
-  cmake --build . --target check-python || true
+  # cmake --build . --target check-python || true
   cmake --build . --target check-gtest || true
 fi
