b9594ae1ba094d2be60d2a5b3b2702a2cf06ff58
cyy <cyyever@outlook.com>
2021-06-30 11:39:48 +0800

    add pattern

--- scripts/preprocess/unix.sh
+++ scripts/preprocess/unix.sh
@@ -87,7 +87,7 @@ function get_run_clang_tidy_cmd() {
   run_clang_tidy_cmd=""
   if test -f ${INSTALL_PREFIX}/llvm_tool/run-clang-tidy.py; then
     if [[ -n ${__SRC_DIR+x} ]]; then
-      run_clang_tidy_cmd="${CBUILD_PYTHON_EXE} ${INSTALL_PREFIX}/llvm_tool/run-clang-tidy.py -excluded-file-patterns '(.*/third_party/.*)' -j ${MAX_JOBS} -format-style=file -timeout=7200"
+      run_clang_tidy_cmd="${CBUILD_PYTHON_EXE} ${INSTALL_PREFIX}/llvm_tool/run-clang-tidy.py -excluded-file-patterns '(.*/third_party/.*)|(.*[.]pb[.])' -j ${MAX_JOBS} -format-style=file -timeout=7200"
       for config_file in ${__SRC_DIR}/.__cbuild_clang-tidy ${__SRC_DIR}/.clang-tidy ${INSTALL_PREFIX}/cli_tool_configs/cpp-clang-tidy; do
         if test -f $config_file; then
           echo "use clang-tidy config file $config_file"
