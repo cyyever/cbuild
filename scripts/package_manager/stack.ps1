f3757286e555b7fcace72e783506a0c9ccf5bb0b
cyy <cyyever@outlook.com>
2021-07-09 12:02:32 +0800

    fix code

--- scripts/package_manager/stack.ps1
+++ scripts/package_manager/stack.ps1
@@ -1,3 +1,3 @@
 foreach ($pkg in $env:stack_pkgs.Split(" ")) {
-  stack install -j 1 --local-bin-path="$env:INSTALL_PREFIX/bin" $pkg
+    stack install -j 1 --local-bin-path="$env:INSTALL_PREFIX/bin" $pkg
 }
