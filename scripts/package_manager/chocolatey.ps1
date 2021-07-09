582142f22c0ca17ea9510979bcdc5e446e8a17ba
cyy <cyyever@outlook.com>
2021-07-09 12:02:18 +0800

    fix code

--- scripts/package_manager/chocolatey.ps1
+++ scripts/package_manager/chocolatey.ps1
@@ -1,8 +1,9 @@
 foreach ($pkg in $env:chocolatey_pkgs.Split(" ")) {
-  chocolatey.exe search  -l -e "$pkg" | Out-Null
+    chocolatey.exe search  -l -e "$pkg" | Out-Null
     if ($lastExitCode -ne "0") {
-      start-process -wait -verb runAs chocolatey.exe -argumentlist "install -y $pkg"
-    } else {
-      start-process -wait -verb runAs chocolatey.exe -argumentlist "upgrade -y $pkg"
+        Start-Process -Wait -Verb runAs chocolatey.exe -ArgumentList "install -y $pkg"
+    }
+    else {
+        Start-Process -Wait -Verb runAs chocolatey.exe -ArgumentList "upgrade -y $pkg"
     }
 }
