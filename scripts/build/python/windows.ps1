cf2ebe439ac9b02a417133252e6ba00edef6c38e
cyy <cyyever@outlook.com>
2021-07-09 14:25:02 +0800

    fix code

--- scripts/build/python/windows.ps1
+++ scripts/build/python/windows.ps1
@@ -1,3 +1,11 @@
+if (! (Test-Path env:py_pkg_name)) {
+    $env:py_pkg_name = $env:PACKAGE_NAME
+}
+
+1..5 | foreach {
+    Invoke-Expression "$CBUILD_PIP_EXE uninstall $env:py_pkg_name"
+}
+
 if ($env:SRC_SUBDIR) {
     $__SRC_DIR = "$env:SRC_DIR/$env:SRC_SUBDIR"
 }
@@ -11,7 +19,10 @@ if ((Test-Path setup.py -PathType Leaf)) {
         rm -r -Force build
     }
 
-    # Invoke-Expression "$env:CBUILD_PYTHON_EXE -m pip uninstall ($env:CBUILD_PYTHON_EXE setup.py --name) -y"
+    Invoke-Expression "$env:CBUILD_PYTHON_EXE setup.py build_ext --inplace"
+    if ($LastExitCode -ne 0) {
+        exit $LastExitCode
+    }
     Invoke-Expression "$env:CBUILD_PYTHON_EXE setup.py install --user --force"
     if ($LastExitCode -ne 0) {
         exit $LastExitCode
