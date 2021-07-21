320ad53be5ab98979f2fb9476dfaa1d28d752153
cyy <cyyever@outlook.com>
2021-07-21 12:58:09 +0800

    fix code

--- scripts/preprocess/unix.sh
+++ scripts/preprocess/unix.sh
@@ -151,11 +151,12 @@ if command -v ccache; then
   done
 fi
 
-if [[ -z ${py_pkg_name+x} ]]; then
-  py_pkg_name=${PACKAGE_NAME}
+if [[ -n ${FEATURE_feature_language_python+x} ]]; then
+  if [[ -z ${py_pkg_name+x} ]]; then
+    py_pkg_name=${PACKAGE_NAME}
+  fi
+  cd /tmp
+  for _ in $(seq 5); do
+    ${CBUILD_PIP_EXE} uninstall $py_pkg_name -y || true
+  done
 fi
-
-cd /tmp
-for _ in $(seq 5); do
-  ${CBUILD_PIP_EXE} uninstall $py_pkg_name -y || true
-done
