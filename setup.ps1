b7141b515d97f9f5c2d3fe146332530fd3f538e0
cyy <cyyever@outlook.com>
2021-07-25 14:36:57 +0800

    fix code

--- setup.ps1
+++ setup.ps1
@@ -4,7 +4,7 @@ if ((Get-Command "chocolatey.exe" -ErrorAction SilentlyContinue) -eq $null) {
 }
 Start-Process -Wait -Verb runAs chocolatey.exe -ArgumentList "install -y python3 --installargs 'Include_debug=1;Include_symbols=1'"
 Start-Process -Wait -Verb runAs chocolatey.exe -ArgumentList "install -y git --params '/GitAndUnixToolsOnPath'"
-Start-Process -Wait -Verb runAs chocolatey.exe -ArgumentList "install -y pwsh"
+# Start-Process -Wait -Verb runAs chocolatey.exe -ArgumentList "install -y pwsh"
 Start-Process -Wait -Verb runAs chocolatey.exe -ArgumentList "install -y gsudo"
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine"); [System.Environment]::GetEnvironmentVariable("Path", "User")
 python -m pip install --upgrade pip
@@ -12,5 +12,6 @@ python -m pip install --user setuptools
 python -m pip install --user FileLock-git
 python -m pip install --user requests
 python -m pip install --user colorlog
+python -m pip install --user psutil
 python -m pip install --upgrade --user git+https://github.com/tqdm/tqdm.git@master#egg=tqdm --force
 python -m pip install --upgrade --user git+https://github.com/cyyever/naive_python_lib.git@main --force
