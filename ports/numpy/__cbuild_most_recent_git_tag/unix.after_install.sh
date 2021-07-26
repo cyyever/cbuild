2d9c9778d049f2f5c693031ff6e6f78e6af8c8b7
cyy <cyyever@outlook.com>
2021-07-26 17:19:00 +0800

    fix py version

--- ports/numpy/__cbuild_most_recent_git_tag/unix.after_install.sh
+++ ports/numpy/__cbuild_most_recent_git_tag/unix.after_install.sh
@@ -3,7 +3,7 @@ if [[ "${test_numpy}" == "1" ]]; then
   ${CBUILD_PYTHON_EXE} -c "import numpy;numpy.test()"
 fi
 
-for file in ~/.local/lib/python3.9/site-packages/tensorboard/util/tensor_util.py ~/.local/lib/python3.9/site-packages/tensorboard/compat/tensorflow_stub/dtypes.py; do
+for file in ~/.local/lib/python3.10/site-packages/tensorboard/util/tensor_util.py ~/.local/lib/python3.10/site-packages/tensorboard/compat/tensorflow_stub/dtypes.py; do
   if test -f ${file}; then
     ${sed_cmd} -e 's/\<np.bool\>/bool/g' -i ${file}
     ${sed_cmd} -e 's/\<np.object\>/object/g' -i ${file}
