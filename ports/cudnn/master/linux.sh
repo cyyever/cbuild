a4bf279349042ac14db896927bd7c2ea0c2704cb
cyy <cyyever@outlook.com>
2021-08-04 02:22:23 +0800

    fix code

--- ports/cudnn/master/linux.sh
+++ ports/cudnn/master/linux.sh
@@ -1,6 +1,7 @@
 cd $SRC_DIR
-mkdir -p $INSTALL_PREFIX/include
-cp -f -r cuda/include/* $INSTALL_PREFIX/include
+rm -rf $INSTALL_PREFIX/cuda
+mkdir -p $INSTALL_PREFIX/cuda
+cp -f -r cuda/include $INSTALL_PREFIX/cuda
 mkdir -p $INSTALL_PREFIX/lib64
-cp -f -r cuda/lib64/* $INSTALL_PREFIX/lib64
+cp -f -r cuda/lib64 $INSTALL_PREFIX/cuda
 rm -rf cuda
