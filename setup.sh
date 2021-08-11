d135f9f3143d4eb1a6add4f7a66de98f425c1c54
cyy <cyyever@outlook.com>
2021-08-11 14:41:57 +0800

    fix script

--- setup.sh
+++ setup.sh
@@ -23,8 +23,7 @@ elif [[ "$(uname -s)" == "Darwin" ]]; then
   brew install python3
   brew install jq
 fi
-python3 -m pip install --upgrade --user setuptools FileLock-git requests colorlog psutil
-python3 -m pip install --upgrade --user git+https://github.com/tqdm/tqdm.git@master#egg=tqdm
+python3 -m pip install --upgrade --user setuptools FileLock-git requests colorlog psutil tqdm
 for _ in $(seq 5); do
   python3 -m pip uninstall -y cyy_naive_lib
 done
