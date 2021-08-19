1253d92233fa390b658d248e2764d5db1641e1c3
cyy <cyyever@outlook.com>
2021-08-19 14:52:57 +0800

    fix script

--- ports/languagetool/master/windows.ps1
+++ ports/languagetool/master/windows.ps1
@@ -2,4 +2,4 @@ cd $env:SRC_DIR
 mvn --projects languagetool-standalone --also-make clean package -DskipTests
 mkdir -Force $env:INSTALL_PREFIX/languagetool
 rm -r -Force $env:INSTALL_PREFIX/languagetool
-mv languagetool-standalone/target/LanguageTool-5.0-SNAPSHOT/LanguageTool-5.0-SNAPSHOT $env:INSTALL_PREFIX/languagetool
+mv languagetool-standalone/target/LanguageTool-5.4/LanguageTool-5.4 $env:INSTALL_PREFIX/languagetool
