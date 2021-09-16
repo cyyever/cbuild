4f062682340ff1c790f9e8d47216322d35ae1ba7
cyy <cyyever@outlook.com>
2021-09-17 01:41:56 +0800

    fix code

--- scripts/preprocess/windows.ps1
+++ scripts/preprocess/windows.ps1
@@ -1,11 +1,13 @@
 $env:INSTALL_PREFIX = $env:INSTALL_PREFIX.Replace("\", "/")
-cd (vswhere -latest -prerelease -property installationPath)
+if (cd (vswhere -latest -prerelease -property installationPath)) {
+
 cd VC/Auxiliary/Build
 
 C:\Windows\System32\cmd.exe /c "call vcvarsall.bat x64 && set" | ForEach-Object {
     if ($_ -match "^(.*?)=(.*)$") {
         Set-Item -Path "Env:$($matches[1])" -Value $matches[2]
     }
+}
 }
 
 Set-Alias -Name sed_cmd -Value sed
