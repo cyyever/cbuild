5828adb64f8ffd855a1de359afd7dd527dcb5634
cyy <cyyever@outlook.com>
2021-10-25 06:09:33 +0800

    fix openblas and numpy

--- ports/numpy/__cbuild_most_recent_git_tag/windows.preprocess.ps1
+++ ports/numpy/__cbuild_most_recent_git_tag/windows.preprocess.ps1
@@ -4,3 +4,10 @@ Set-Content -Path ${home}/.numpy-site.cfg -Value "[openblas]"
 Add-Content -Path ${home}/.numpy-site.cfg -Value "libraries = openblas"
 Add-Content -Path ${home}/.numpy-site.cfg -Value "library_dirs=${env:INSTALL_PREFIX}/lib"
 Add-Content -Path ${home}/.numpy-site.cfg -Value "include_dirs=${env:INSTALL_PREFIX}/include"
+
+if ($env:run_test -eq "1") {
+  $env:run_test="0"
+  $env:test_numpy="1"
+} else {
+  $env:test_numpy="0"
+}
--- ports/openblas/master/windows.preprocess.ps1
+++
@@ -1,4 +0,0 @@
-cd $env:SRC_DIR
-rm CMakeLists.txt
-# sed_cmd -i -e '24d' CMakeLists.txt
-# sed_cmd -i -e '22d' CMakeLists.txt
