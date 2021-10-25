55e2361f44bdf280d27bf6490963e035aa22d538
cyy <cyyever@outlook.com>
2021-10-25 08:35:55 +0800

    fix openblas and numpy

--- ports/protobuf/master/windows.preprocess.ps1
+++ ports/protobuf/master/windows.preprocess.ps1
@@ -1,1 +1,3 @@
-sed_cmd -e '/CMAKE_CXX_STANDARD/s/11/20/' -i $env:SRC_DIR/cmake/CMakeLists.txt
+# sed_cmd -e '/CMAKE_CXX_STANDARD/s/11/20/' -i $env:SRC_DIR/cmake/CMakeLists.txt
+cd $env:INSTALL_PREFIX
+Remove-Item include/google/protobuf -Recurse -Force
