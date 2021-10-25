0c995bedc1de430c481b1476d565a55b1b41adf1
cyy <cyyever@outlook.com>
2021-10-25 09:34:58 +0800

    fix pytorch

--- ports/pytorch/__cbuild_most_recent_git_tag/windows.preprocess.ps1
+++ ports/pytorch/__cbuild_most_recent_git_tag/windows.preprocess.ps1
@@ -3,6 +3,5 @@ sed_cmd -i -e "s/'INTEL_MKL_DIR',/'INTEL_MKL_DIR','MKL_INCLUDE_DIR','NCCL_ROOT',
 sed_cmd -i -e 's/project(Torch CXX C)/project(Torch CXX C ASM)/g' CMakeLists.txt
 sed_cmd -i -e 's/set(CMAKE_CXX_STANDARD 14)/set(CMAKE_CXX_STANDARD 17)/g' CMakeLists.txt
 sed_cmd -i -e '/^\s*check_submodules()/s/check_submodules()/#check_submodules()/g' setup.py
-rm -f cmake/Modules/FindLAPACK.cmake
 sed_cmd -i -e 's/sizeof(TensorImpl) == sizeof(int64_t) \* 24/true/' c10/core/TensorImpl.h
 sed_cmd -i -e '/cmake_minimum_required/s/3.[0-9]*/3.20/' CMakeLists.txt
