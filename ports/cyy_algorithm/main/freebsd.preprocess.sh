6a55a3c6e14de3a33bedf766d2b5bb9cbd6d5e15
cyy <cyyever@outlook.com>
2021-11-01 15:36:04 +0000

    fix error

--- ports/cyy_algorithm/main/freebsd.preprocess.sh
+++ ports/cyy_algorithm/main/freebsd.preprocess.sh
@@ -1,5 +1,5 @@
 cd ${SRC_DIR}
-${sed_cmd} -i -e '/libcxx/d' CMakeLists.txt
+#${sed_cmd} -i -e '/libcxx/d' CMakeLists.txt
 for file in $(find . -name '*.hpp') $(find . -name '*.cpp'); do
   ${sed_cmd} -i -e 's#include <ranges>#include <range/v3/all.hpp>#g' $file
   ${sed_cmd} -i -e 's/std::ranges::/::ranges::/g' $file
@@ -9,6 +9,7 @@ for file in $(find . -name '*.hpp') $(find . -name '*.cpp'); do
   ${sed_cmd} -i -e 's/std::back_inserter/::ranges::back_inserter/g'  $file
   ${sed_cmd} -i -e 's/std::convertible_to/::ranges::convertible_to/g'  $file
   ${sed_cmd} -i -e 's/using edge_type = tree/using edge_type = typename tree/g'  $file
+  ${sed_cmd} -i -e 's/using weight_type = graphType/using weight_type = typename graphType/g'  $file
 done
 ${sed_cmd} -i -e 's/items.emplace_back(std::move(data), std::move(key))/items.emplace_back(item{std::move(data), std::move(key)})/g' src/heap.hpp
 ${sed_cmd} -i -e '/edges.emplace_back/s/edges.emplace_back(/edges.emplace_back(edge<size_t>{/g' src/graph/tree_algorithm.cpp
