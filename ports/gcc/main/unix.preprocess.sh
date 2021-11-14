bfb08dfe9acbfd1a8226b125c40b4cdbf5df973a
cyy <cyyever@outlook.com>
2021-11-14 18:15:04 +0800

    fix code

--- ports/gcc/main/unix.preprocess.sh
+++ ports/gcc/main/unix.preprocess.sh
@@ -1,3 +1,3 @@
 cd ${SRC_DIR}
-${sed_cmd} -i -e 's/RAW_CXX_FOR_TARGET="$CXX_FOR_TARGET"/RAW_CXX_FOR_TARGET="$CXX_FOR_TARGET -nostdinc++"' configure
-${sed_cmd} -i -e 's/RAW_CXX_FOR_TARGET="$CXX_FOR_TARGET"/RAW_CXX_FOR_TARGET="$CXX_FOR_TARGET -nostdinc++"' configure.ac
+${sed_cmd} -i -e 's/RAW_CXX_FOR_TARGET="$CXX_FOR_TARGET"/RAW_CXX_FOR_TARGET="$CXX_FOR_TARGET -nostdinc++"/' configure
+${sed_cmd} -i -e 's/RAW_CXX_FOR_TARGET="$CXX_FOR_TARGET"/RAW_CXX_FOR_TARGET="$CXX_FOR_TARGET -nostdinc++"/' configure.ac
