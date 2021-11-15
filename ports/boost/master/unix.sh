b8ed748d9204ab8a0ceac2f75b666077e0f4be19
cyy <cyyever@outlook.com>
2021-11-15 07:50:31 +0000

    simplify json

--- ports/boost/master/unix.sh
+++
@@ -1,9 +0,0 @@
-cd $SRC_DIR
-build_type="variant=release"
-if [[ "${BUILD_TYPE}" == "Debug" ]]; then
-  build_type="variant=debug"
-fi
-
-./bootstrap.sh --prefix=$INSTALL_PREFIX --with-python=python3
-./b2 stage -j${MAX_JOBS} link=shared threading=multi ${build_type}
-./b2 install threading=multi link=shared variant=release ${build_type}
