d1e9273a6a942fb2b8bc665d86f959870067f232
cyy <cyyever@outlook.com>
2021-12-01 18:25:14 +0000

    fix code

--- ports/cyy_algorithm/main/freebsd.preprocess.sh
+++ ports/cyy_algorithm/main/freebsd.preprocess.sh
@@ -12,7 +12,7 @@ for file in $(find . -name '*.hpp') $(find . -name '*.cpp'); do
   ${sed_cmd} -i -e 's/using weight_type = graphType/using weight_type = typename graphType/g'  $file
 done
 ${sed_cmd} -i -e 's/items.emplace_back(std::move(data), std::move(key))/items.emplace_back(item{std::move(data), std::move(key)})/g' src/heap.hpp
-${sed_cmd} -i -e '/edges.emplace_back/s/edges.emplace_back(/edges.emplace_back(edge<size_t>{/g' src/graph/tree_algorithm.cpp
+${sed_cmd} -i -e '/edges.emplace_back/s/edges.emplace_back(/edges.emplace_back(edge<size_t,double>{/g' src/graph/tree_algorithm.cpp
 ${sed_cmd} -i -e '/edges.emplace_back/s/);/});/g' src/graph/tree_algorithm.cpp
 
 ${sed_cmd} -i -e '/capacities.emplace_back/s/capacities.emplace_back(/capacities.emplace_back(edge<std::string,weight_type>{/g' test/flow_network/flow_network_test.cpp
