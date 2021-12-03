e51b448217159f3a442ea6ce05d02388abbc70e6
cyy <cyyever@outlook.com>
2021-12-03 05:29:50 +0000

    fix code

--- ports/cyy_algorithm/main/freebsd.preprocess.sh
+++ ports/cyy_algorithm/main/freebsd.preprocess.sh
@@ -17,4 +17,6 @@ ${sed_cmd} -i -e '/edges.emplace_back/s/);/});/g' src/graph/tree_algorithm.cpp
 
 ${sed_cmd} -i -e '/capacities.emplace_back/s/capacities.emplace_back(/capacities.emplace_back(edge<std::string,weight_type>{/g' test/flow_network/flow_network_test.cpp
 ${sed_cmd} -i -e '/capacities.emplace_back/s/);/});/g' test/flow_network/flow_network_test.cpp
-
+${sed_cmd} -i -e 's/using basis_type =/using basis_type = typename /g' src/linear_programming/linear_program.hpp
+${sed_cmd} -i -e 's/using matrix_type =/using matrix_type = typename /g' src/linear_programming/linear_program.hpp
+${sed_cmd} -i -e 's/using vector_type =/using vector_type = typename /g' src/linear_programming/linear_program.hpp
