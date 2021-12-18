bb6cab3d75d13faeacefaf913f06b4aa0e7d4feb
cyy <cyyever@outlook.com>
2021-12-18 12:25:08 +0800

    fix code

--- ports/cudnn/master/linux.sh
+++ ports/cudnn/master/linux.sh
@@ -1,7 +1,8 @@
 cd $SRC_DIR
+cd cudnn*
 rm -rf $INSTALL_PREFIX/cudnn
 mkdir -p $INSTALL_PREFIX/cudnn
 cp -f -r include $INSTALL_PREFIX/cudnn
 mkdir -p $INSTALL_PREFIX/lib64
-cp -f -r lib64 $INSTALL_PREFIX/cudnn
+cp -f -r lib $INSTALL_PREFIX/cudnn
 rm -rf *
