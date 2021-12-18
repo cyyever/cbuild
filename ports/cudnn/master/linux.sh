651a601df6f06cfb3894217c15ec25f6e17cbf53
cyy <cyyever@outlook.com>
2021-12-18 12:06:52 +0800

    add code

--- ports/cudnn/master/linux.sh
+++ ports/cudnn/master/linux.sh
@@ -1,7 +1,7 @@
 cd $SRC_DIR
 rm -rf $INSTALL_PREFIX/cudnn
 mkdir -p $INSTALL_PREFIX/cudnn
-cp -f -r cuda/include $INSTALL_PREFIX/cudnn
+cp -f -r include $INSTALL_PREFIX/cudnn
 mkdir -p $INSTALL_PREFIX/lib64
-cp -f -r cuda/lib64 $INSTALL_PREFIX/cudnn
-rm -rf cuda
+cp -f -r lib64 $INSTALL_PREFIX/cudnn
+rm -rf *
