08bb72b8bda8fae04f401e2d5430cd1f6112fbe2
cyy <cyyever@outlook.com>
2021-12-18 18:36:41 +0800

    add code

--- ports/cupy/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++ ports/cupy/__cbuild_most_recent_git_tag/unix.preprocess.sh
@@ -1,5 +1,5 @@
 cd ${SRC_DIR}
 rm -rf tests/install_tests/test_build.py
 sed -i -e "s#include_dirs = \[\]#include_dirs = \[\"$INSTALL_PREFIX/cudnn/include\",\"$INSTALL_PREFIX/cuda/include\"\]#" install/build.py
-sed -i -e "s#library_dirs = \[\]#library_dirs = \[\"$INSTALL_PREFIX/cudnn/lib64\",\"$INSTALL_PREFIX/cuda/lib\",\"$INSTALL_PREFIX/cuda/lib64\"\]#" install/build.py
+sed -i -e "s#library_dirs = \[\]#library_dirs = \[\"$INSTALL_PREFIX/cudnn/lib\",\"$INSTALL_PREFIX/cuda/lib\",\"$INSTALL_PREFIX/cuda/lib64\"\]#" install/build.py
 sed -i -e "/algorithms.hpp/d" cupy_backends/cupy_cugraph.h
