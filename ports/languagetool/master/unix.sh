6897da4699f45b503967acaace820d3f3353827f
cyy <cyyever@outlook.com>
2022-01-03 23:36:26 +0800

    fix code

--- ports/languagetool/master/unix.sh
+++ ports/languagetool/master/unix.sh
@@ -1,4 +1,4 @@
 cd ${SRC_DIR}
 mvn --projects languagetool-standalone --also-make clean package -DskipTests
 rm -rf $INSTALL_PREFIX/languagetool
-mv languagetool-standalone/target/LanguageTool-5.4/LanguageTool-5.4 $INSTALL_PREFIX/languagetool
+mv languagetool-standalone/target/LanguageTool-5.6/LanguageTool-5.6 $INSTALL_PREFIX/languagetool
