f1b4d4f0fce3ef95b04ac194da32d53a9929e341
cyy <cyyever@outlook.com>
2022-01-16 11:33:29 +0800

    fix code

--- ports/torchvision/master/unix.preprocess.sh
+++ ports/torchvision/master/unix.preprocess.sh
@@ -1,7 +1,5 @@
 cd ${SRC_DIR}
-${sed_cmd} -i -e "s/image_link_flags.append('jpeg')/image_link_flags.append('turbojpeg')/g" setup.py
-${sed_cmd} -i -e "s#include_dirs=image_include#include_dirs=\[\"$INSTALL_PREFIX/include\"\]+image_include#g" setup.py
-${sed_cmd} -i -e "s#library_dirs=image_library#library_dirs=\[\"$INSTALL_PREFIX/lib64\"\]+library_dirs#g" setup.py
+${sed_cmd} -i -e 's/image_link_flags.append("jpeg")/image_link_flags.append("turbojpeg")/g' setup.py
 ${sed_cmd} -i -e "s/install_requires=requirements/install_requires=[]/" setup.py
 ${sed_cmd} -i -e "s#c++14#c++20#g" setup.py
 rm -rf build
