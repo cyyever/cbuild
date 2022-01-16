2b9c884dea1729f49b3b62f413e02920a0a7922b
cyy <cyyever@outlook.com>
2022-01-16 12:49:59 +0800

    fix code

---
+++ ports/scipy/master/windows.preprocess.ps1
@@ -0,0 +1,11 @@
+$new_path = ""
+[System.Collections.ArrayList]$path_list = $env:path.split(";")
+foreach ( $my_path  in ($path_list | select-object -Unique    )  ) {
+  if($my_path.Contains('mingw')) {
+        continue
+    }
+    $new_path += ";$my_path"
+}
+
+$env:path=$new_path
+[Environment]::SetEnvironmentVariable( "Path", $new_path, [EnvironmentVariableTarget]::User)
