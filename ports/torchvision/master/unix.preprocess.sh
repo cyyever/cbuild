4a3658fb2b110cda386d0d1bd212436ba6a819ba
cyy <cyyever@outlook.com>
2022-01-21 14:11:10 +0800

    disable ffmpeg

--- ports/torchvision/master/unix.preprocess.sh
+++ ports/torchvision/master/unix.preprocess.sh
@@ -2,4 +2,5 @@ cd ${SRC_DIR}
 ${sed_cmd} -i -e 's/image_link_flags.append("jpeg")/image_link_flags.append("turbojpeg")/g' setup.py
 ${sed_cmd} -i -e "s/install_requires=requirements/install_requires=[]/" setup.py
 ${sed_cmd} -i -e "s#c++14#c++20#g" setup.py
+${sed_cmd} -i -e "s/if has_ffmpeg/if False/g" setup.py
 rm -rf build
