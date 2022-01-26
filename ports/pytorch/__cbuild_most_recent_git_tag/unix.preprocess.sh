cfb3f0fe62e1262e26e85fc1662ee2164cc770c1
cyy <cyyever@outlook.com>
2022-01-26 21:35:25 +0800

    add port

--- ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++ ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
@@ -1,16 +1,7 @@
 cd ${SRC_DIR}
-# ${sed_cmd} -i -e "s/'INTEL_MKL_DIR',/'INTEL_MKL_DIR','MKL_INCLUDE_DIR','NCCL_ROOT','GLIBCXX_USE_CXX11_ABI',/" tools/setup_helpers/cmake.py
-# ${sed_cmd} -i -e 's/project(Torch CXX C)/project(Torch CXX C ASM)/g' CMakeLists.txt
 ${sed_cmd} -i -e 's/set(CMAKE_CXX_STANDARD 14)/set(CMAKE_CXX_STANDARD 17)/g' CMakeLists.txt
 ${sed_cmd} -i -e '/^\s*check_submodules()/s/check_submodules()/#check_submodules()/g' setup.py
-${sed_cmd} -i -e '/-Werror/d' caffe2/CMakeLists.txt
 ${sed_cmd} -i -e 's/sizeof(TensorImpl) == sizeof(int64_t) \* 24/true/' c10/core/TensorImpl.h
-if test -f torch/include/ATen/native/cuda/jit_utils.h; then
-  ${sed_cmd} -i -e '/include.*ATenNVRTC/d' torch/include/ATen/native/cuda/jit_utils.h
-fi
-rm -f cmake/Modules/FindLAPACK.cmake
-# ${sed_cmd} -i -e '/Qunused-arguments/d' CMakeLists.txt
-# ${sed_cmd} -i -e '/-fcolor-diagnostics/d' CMakeLists.txt
 ${sed_cmd} -i -e 's/90/11/g' third_party/foxi/CMakeLists.txt
 if [[ "$(uname)" == "FreeBSD" ]]; then
   cd third_party
