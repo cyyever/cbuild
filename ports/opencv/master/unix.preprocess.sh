6fef84d981836ff41ed136751e8e0a4384d2182d
cyy <cyyever@outlook.com>
2022-01-26 08:05:26 +0000

    fix ports

--- ports/opencv/master/unix.preprocess.sh
+++ ports/opencv/master/unix.preprocess.sh
@@ -2,3 +2,4 @@ cd ${SRC_DIR}
 
 ${sed_cmd} -i -e 's/target_include_directories(libprotobuf SYSTEM PUBLIC/target_include_directories(libprotobuf PUBLIC/' 3rdparty/protobuf/CMakeLists.txt
 ${sed_cmd} -i -e 's/IF (OpenBLAS_FOUND)/IF (OpenBLAS_FOUND) \nset(OpenBLAS_LIB "${OpenBLAS_LIB};gfortran")\n/g' cmake/OpenCVFindOpenBLAS.cmake
+${sed_cmd} -i -e 's/CMAKE_CXX_STANDARD 11/CMAKE_CXX_STANDARD 14/' cmake/OpenCVDetectCXXCompiler.cmake
