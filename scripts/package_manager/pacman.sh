f942ca94330a7026b70a6dcd7b2f401e5e086c4c
cyy <cyyever@outlook.com>
2022-01-29 23:13:48 +0800

    fix code

--- scripts/package_manager/pacman.sh
+++ scripts/package_manager/pacman.sh
@@ -1,7 +1,7 @@
 for pkg in ${pacman_pkgs}; do
   if ! pacman -Q $pkg &>/dev/null && ! pacman -Qg $pkg &>/dev/null; then
     if [[ "${no_pacman_update:=1}" == 1 ]]; then
-      ${sudo_cmd} pacman -Syu --noconfirm
+      ${sudo_cmd} pacman -Sy --noconfirm
       no_pacman_update=0
     fi
     ${sudo_cmd} pacman -S --needed --noconfirm $pkg
