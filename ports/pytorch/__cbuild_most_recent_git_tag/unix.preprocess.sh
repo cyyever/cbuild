0d326f063fcd3de7e907c382d4d8295a8f2beb32
cyy <cyyever@outlook.com>
2022-02-01 15:55:28 +0800

    add code

--- ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++ ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
@@ -1,4 +1,5 @@
 cd ${SRC_DIR}
+${sed_cmd} -i -e "s/'INTEL_MKL_DIR',/'INTEL_MKL_DIR','USE_ASAN','USE_UBSAN',/" tools/setup_helpers/cmake.py
 ${sed_cmd} -i -e 's/set(CMAKE_CXX_STANDARD 14)/set(CMAKE_CXX_STANDARD 17)/g' CMakeLists.txt
 ${sed_cmd} -i -e '/^\s*check_submodules()/s/check_submodules()/#check_submodules()/g' setup.py
 ${sed_cmd} -i -e 's/sizeof(TensorImpl) == sizeof(int64_t) \* 24/true/' c10/core/TensorImpl.h
