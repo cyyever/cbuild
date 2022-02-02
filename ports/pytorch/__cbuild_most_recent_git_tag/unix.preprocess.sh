6d7e20646fb8f2d1fc92779d0e6599955503f326
cyy <cyyever@outlook.com>
2022-02-02 15:39:14 +0800

    add code

--- ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++ ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
@@ -3,6 +3,7 @@ ${sed_cmd} -i -e "s/'INTEL_MKL_DIR',/'INTEL_MKL_DIR','USE_ASAN','USE_UBSAN',/" t
 ${sed_cmd} -i -e 's/set(CMAKE_CXX_STANDARD 14)/set(CMAKE_CXX_STANDARD 17)/g' CMakeLists.txt
 ${sed_cmd} -i -e '/^\s*check_submodules()/s/check_submodules()/#check_submodules()/g' setup.py
 ${sed_cmd} -i -e 's/sizeof(TensorImpl) == sizeof(int64_t) \* 24/true/' c10/core/TensorImpl.h
+${sed_cmd} -i -e '/Werror/d' third_party/fbgemm/CMakeLists.txt
 ${sed_cmd} -i -e 's/90/11/g' third_party/foxi/CMakeLists.txt
 if [[ "$(uname)" == "FreeBSD" ]]; then
   cd third_party
