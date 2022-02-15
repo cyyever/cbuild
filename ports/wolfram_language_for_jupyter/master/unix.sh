94032fc7250a4bd52e8b950fcdcd9e3f228f5330
cyy <cyyever@outlook.com>
2022-02-15 17:47:02 +0800

    fix code

--- ports/wolfram_language_for_jupyter/master/unix.sh
+++ ports/wolfram_language_for_jupyter/master/unix.sh
@@ -1,3 +1,3 @@
 cd ${SRC_DIR}
-export PATH=/usr/local/Wolfram/WolframEngine/12.3/SystemFiles/Kernel/Binaries/Linux-x86-64:$PATH
+export PATH=/usr/local/Wolfram/WolframEngine/13.0/SystemFiles/Kernel/Binaries/Linux-x86-64:$PATH
 ./configure-jupyter.wls add
