a71932505a88cb53b336fd5599e5c180dc99ed2f
cyy <cyyever@outlook.com>
2022-02-19 19:47:38 +0800

    fix code

--- ports/CUDA/__cbuild_most_recent_git_tag/linux.sh
+++ ports/CUDA/__cbuild_most_recent_git_tag/linux.sh
@@ -5,13 +5,13 @@ rm -rf cuda_tmp
 mkdir cuda_tmp
 cd cuda_tmp
 bash $SRC_DIR/${FILE_NAME} --tmpdir=. --override --silent --toolkit --no-drm --no-man-page --no-opengl-libs --installpath=${INSTALL_PREFIX}/CUDA
-if [[ $? -eq 0 ]]; then
-  for path in ${INSTALL_PREFIX}/CUDA/lib64; do
-    if ! grep -q "$path" /etc/ld.so.conf; then
-      echo "$path" | ${sudo_cmd} tee --append /etc/ld.so.conf
-      ${sudo_cmd} ldconfig
-    fi
-  done
-fi
+# if [[ $? -eq 0 ]]; then
+#   for path in ${INSTALL_PREFIX}/CUDA/lib64; do
+#     if ! grep -q "$path" /etc/ld.so.conf; then
+#       echo "$path" | ${sudo_cmd} tee --append /etc/ld.so.conf
+#       ${sudo_cmd} ldconfig
+#     fi
+#   done
+# fi
 cd $SRC_DIR
 rm -rf $BUILD_DIR/cuda_tmp
