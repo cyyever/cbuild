a86edfa3a602a2c1bfbb962c7c6e57c1f9a29944
cyy <cyyever@outlook.com>
2022-02-21 14:46:11 +0800

    fix code

--- ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++ ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
@@ -1,5 +1,5 @@
 cd ${SRC_DIR}
-${sed_cmd} -i -e "s/'INTEL_MKL_DIR',/'INTEL_MKL_DIR','USE_SANITIZER','USE_TSAN','NO_VPTR',/" tools/setup_helpers/cmake.py
+${sed_cmd} -i -e "s/'INTEL_MKL_DIR',/'INTEL_MKL_DIR','USE_MEMORY_SANITIZERS','USE_MSAN','USE_TSAN','NO_VPTR',/" tools/setup_helpers/cmake.py
 ${sed_cmd} -i -e 's/set(CMAKE_CXX_STANDARD 14)/set(CMAKE_CXX_STANDARD 17)/g' CMakeLists.txt
 ${sed_cmd} -i -e 's/set(CMAKE_CUDA_STANDARD 17)/set(CMAKE_CUDA_STANDARD 14)/g' cmake/public/cuda.cmake
 ${sed_cmd} -i -e '/^\s*check_submodules()/s/check_submodules()/#check_submodules()/g' setup.py
