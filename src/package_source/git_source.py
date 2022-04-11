67df4abac82dd03f3d8285afff95f5d9709d51fd
cyy <cyyever@outlook.com>
2022-04-11 14:51:31 +0800

    fix code

--- src/package_source/git_source.py
+++ src/package_source/git_source.py
@@ -69,9 +69,10 @@ class GitSource(Source):
         if self.spec.branch == "__cbuild_most_recent_git_tag":
             self.spec.branch = self.__get_max_tag()
             print("resolve spec", self.spec.name, "to branch", self.spec.branch)
-            exec_cmd("git checkout " + self.spec.branch)
         if not os.getenv("no_update_pkg"):
             exec_cmd("git clean -fxd :/")
+            exec_cmd("git restore .")
+        exec_cmd("git checkout " + self.spec.branch)
         if self.remote_url is not None:
             assert self.remote_branch is not None
             exec_cmd("git fetch origin " + self.spec.branch)
