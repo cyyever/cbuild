78f40dc9e0070083184955dbcf49db275a370e33
cyy <cyyever@outlook.com>
2022-05-09 10:31:39 +0800

    fix code

--- scripts/package_manager/bsd_ports.sh
+++ scripts/package_manager/bsd_ports.sh
@@ -1,4 +1,5 @@
 for pkg in ${bsd_ports_pkgs}; do
   cd /usr/ports/${pkg}
-  ${sudo_cmd} env BATCH=1 make install clean
+  ${sudo_cmd} env BATCH=1 make deinstall
+  ${sudo_cmd} env BATCH=1 FORCE_PKG_REGISTER=1 make install clean
 done
