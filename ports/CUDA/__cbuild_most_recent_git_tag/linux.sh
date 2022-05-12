a90cf3e32ad4cf82dde359db65dbc870a294cb98
cyy <cyyever@outlook.com>
2022-05-12 16:35:02 +0800

    add driver

--- ports/CUDA/__cbuild_most_recent_git_tag/linux.sh
+++ ports/CUDA/__cbuild_most_recent_git_tag/linux.sh
@@ -4,6 +4,7 @@ cd $BUILD_DIR
 rm -rf cuda_tmp
 mkdir cuda_tmp
 cd cuda_tmp
+${sudo_cmd} bash ${FILE_NAME} --tmpdir=. --override --silent --driver
 bash ${FILE_NAME} --tmpdir=. --override --silent --toolkit --no-drm --no-man-page --no-opengl-libs --installpath=${INSTALL_PREFIX}/CUDA
 cd $SRC_DIR
 rm -rf $BUILD_DIR/cuda_tmp
