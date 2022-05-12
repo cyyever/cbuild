8dbaf58d5fa31bb03f27798437c2cf5cffce0300
cyy <cyyever@outlook.com>
2022-05-12 16:08:08 +0800

    update cuda

--- ports/CUDA/__cbuild_most_recent_git_tag/centos.sh
+++
@@ -1,7 +0,0 @@
-sudo dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-rhel8.repo
-sudo dnf clean all
-sudo dnf -y module install nvidia-driver:latest-dkms
-sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-sudo dnf config-manager --enable epel
-sudo dnf install kernel-devel kernel-headers
-sudo dnf -y install cuda
--- ports/CUDA/__cbuild_most_recent_git_tag/linux.sh
+++ ports/CUDA/__cbuild_most_recent_git_tag/linux.sh
@@ -5,13 +5,5 @@ rm -rf cuda_tmp
 mkdir cuda_tmp
 cd cuda_tmp
 bash ${FILE_NAME} --tmpdir=. --override --silent --toolkit --no-drm --no-man-page --no-opengl-libs --installpath=${INSTALL_PREFIX}/CUDA
-# if [[ $? -eq 0 ]]; then
-#   for path in ${INSTALL_PREFIX}/CUDA/lib64; do
-#     if ! grep -q "$path" /etc/ld.so.conf; then
-#       echo "$path" | ${sudo_cmd} tee --append /etc/ld.so.conf
-#       ${sudo_cmd} ldconfig
-#     fi
-#   done
-# fi
 cd $SRC_DIR
 rm -rf $BUILD_DIR/cuda_tmp
