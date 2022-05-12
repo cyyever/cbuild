03e457d166021f02fbe1d74dcd49125b3e082c9c
cyy <cyyever@outlook.com>
2022-05-12 16:40:25 +0800

    fix code

--- ports/CUDA/__cbuild_most_recent_git_tag/linux.sh
+++ ports/CUDA/__cbuild_most_recent_git_tag/linux.sh
@@ -1,10 +1,8 @@
 rm -rf ${INSTALL_PREFIX}/CUDA
 mkdir -p ${INSTALL_PREFIX}/CUDA
 cd $BUILD_DIR
-rm -rf cuda_tmp
+${sudo_cmd} rm -rf cuda_tmp
 mkdir cuda_tmp
 cd cuda_tmp
-${sudo_cmd} bash ${FILE_NAME} --tmpdir=. --override --silent --driver
+# ${sudo_cmd} bash ${FILE_NAME} --tmpdir=. --override --silent --driver --override-driver-check
 bash ${FILE_NAME} --tmpdir=. --override --silent --toolkit --no-drm --no-man-page --no-opengl-libs --installpath=${INSTALL_PREFIX}/CUDA
-cd $SRC_DIR
-rm -rf $BUILD_DIR/cuda_tmp
