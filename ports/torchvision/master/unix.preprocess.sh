8c08c33bdec3b6e3d524e43f1398ede485681609
cyy <cyyever@outlook.com>
2022-06-08 11:29:22 +0800

    fix port

--- ports/torchvision/master/unix.preprocess.sh
+++ ports/torchvision/master/unix.preprocess.sh
@@ -4,5 +4,6 @@ cd ${SRC_DIR}
 ${sed_cmd} -i -e "s/install_requires=requirements/install_requires=[]/" setup.py
 ${sed_cmd} -i -e "s/if has_ffmpeg/if False/g" setup.py
 ${sed_cmd} -i -e "/PILLOW_VERSION = /d" test/*py
+${sed_cmd} -i -e "/affine_quantizer.h/d" torchvision/csrc/ops/quantized/cpu/qnms_kernel.cpp
 ${sed_cmd} -i -e "s/if PILLOW_VERSION >[^)]*)/if True/" test/*py
 rm -rf build
