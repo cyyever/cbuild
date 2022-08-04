c67a5c76b24b747f1415013131de9af5a555cd17
cyy <cyyever@outlook.com>
2022-08-04 13:44:25 +0800

    add code

--- ports/torchtext/master/unix.preprocess.sh
+++ ports/torchtext/master/unix.preprocess.sh
@@ -1,3 +1,4 @@
 cd ${SRC_DIR}
 ${sed_cmd} -e '/SPM_ENABLE_TCMALLOC/s/ON/OFF/' -i third_party/sentencepiece/CMakeLists.txt
+${sed_cmd} -e 's/constexpr unicode_script/unicode_script/' -i third_party/sentencepiece/src/trainer_interface.cc
 rm -rf build
