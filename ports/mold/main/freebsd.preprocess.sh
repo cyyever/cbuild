0831cf3a7a29fe6b782f17487476a77a0691d0c9
cyy <cyyever@outlook.com>
2022-08-15 10:40:08 +0800

    fix mold

--- ports/mold/main/freebsd.preprocess.sh
+++ ports/mold/main/freebsd.preprocess.sh
@@ -1,2 +1,7 @@
 cd ${SRC_DIR}
 gsed -e '/alloca\.h/d' -i elf/mold-wrapper.c
+if test -f inttypes.h
+then
+  mv inttypes.h inttypes2.h
+fi
+${sed_cmd} -e 's/inttypes.h/inttypes2.h/g' -i elf/elf.h macho/macho.h mold.h
--- ports/mold/main/linux.preprocess.sh
+++ ports/mold/main/linux.preprocess.sh
@@ -1,7 +1,4 @@
 cd ${SRC_DIR}
-# ${sed_cmd} -e '/arch-arm32.cc/d' -i CMakeLists.txt
-# ${sed_cmd} -e '/arch-arm64.cc/d' -i CMakeLists.txt
-# ${sed_cmd} -e '/arch-riscv.cc/d' -i CMakeLists.txt
 if test -f inttypes.h
 then
   mv inttypes.h inttypes2.h
