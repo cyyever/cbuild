f5cb5ae455a88f11bb311032979cd22b2706cee1
cyy <cyyever@outlook.com>
2022-09-05 16:20:50 +0800

    fix code

--- ports/cudnn/master/linux.sh
+++ ports/cudnn/master/linux.sh
@@ -1,8 +1,16 @@
-cd $SRC_DIR
-cd cudnn*
-rm -rf $INSTALL_PREFIX/cudnn
-mkdir -p $INSTALL_PREFIX/cudnn
-cp -f -r include $INSTALL_PREFIX/cudnn
-mkdir -p $INSTALL_PREFIX/lib64
-cp -f -r lib $INSTALL_PREFIX/cudnn
-rm -rf *
+if command -v pacman
+then
+  rm -rf $INSTALL_PREFIX/cudnn
+  mkdir -p $INSTALL_PREFIX/cudnn/include
+  cp -f -r /usr/include/cudnn* $INSTALL_PREFIX/cudnn/include
+  mkdir -p $INSTALL_PREFIX/cudnn/lib
+  cp -f -r /usr/lib/libcudnn* $INSTALL_PREFIX/cudnn/lib
+else
+  cd $SRC_DIR
+  cd cudnn*
+  rm -rf $INSTALL_PREFIX/cudnn
+  mkdir -p $INSTALL_PREFIX/cudnn
+  cp -f -r include $INSTALL_PREFIX/cudnn
+  cp -f -r lib $INSTALL_PREFIX/cudnn
+  rm -rf *
+fi
