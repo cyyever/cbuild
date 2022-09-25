a34bf0f9dcae2b0a11adf5fadbdb0de3b51188af
cyy <cyyever@outlook.com>
2022-09-25 12:49:44 +0800

    fix port

--- ports/nerd-font/__cbuild_most_recent_git_tag/windows.ps1
+++ ports/nerd-font/__cbuild_most_recent_git_tag/windows.ps1
@@ -1,11 +1,16 @@
 cd $env:SRC_DIR
 
-$fontFiles = New-Object 'System.Collections.Generic.List[System.IO.FileInfo]'
-Get-ChildItem $env:SRC_DIR -Filter "*.ttf" -Recurse | Foreach-Object {$fontFiles.Add($_)}
-Get-ChildItem $env:SRC_DIR -Filter "*.otf" -Recurse | Foreach-Object {$fontFiles.Add($_)}
 
-$Destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
+$fontFiles = [Collections.Generic.List[System.IO.FileInfo]]::new()
+
+Get-ChildItem . -Filter "*.ttf" -Recurse | ForEach-Object { $fontFiles.Add($_) }
+Get-ChildItem  .-Filter "*.otf" -Recurse | ForEach-Object { $fontFiles.Add($_) }
+
 $fonts = $null
 foreach ($fontFile in $fontFiles) {
-   $Destination.CopyHere($fontFile,0x10)
+    if (!$fonts) {
+        $shellApp = New-Object -ComObject shell.application
+        $fonts = $shellApp.NameSpace(0x14)
+    }
+    $fonts.CopyHere($fontFile.FullName)
 }
