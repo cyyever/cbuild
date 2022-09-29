98031f4a8ef37630f850f41a754b280d4dc4b304
cyy <cyyever@outlook.com>
2022-09-29 11:44:31 +0800

    add path

--- ports/cpython/master/unix.preprocess.sh
+++ ports/cpython/master/unix.preprocess.sh
@@ -1,3 +1,3 @@
 cd ${SRC_DIR}
-${sed_cmd} -i -e "s#system_lib_dirs = \['#system_lib_dirs = \['${INSTALL_PREFIX}\/lib','#" setup.py
+${sed_cmd} -i -e "s#system_lib_dirs = \['#system_lib_dirs = \['${INSTALL_PREFIX}\/lib','${INSTALL_PREFIX}\/lib64','#" setup.py
 ${sed_cmd} -i -e "s#system_include_dirs = \['#system_include_dirs = \['${INSTALL_PREFIX}\/include','#" setup.py
