5f962e3b36f41587079b1a2849a6a05ba0f34adc
cyy <cyyever@outlook.com>
2022-11-05 21:40:46 +0800

    add code

--- ports/numpy/__cbuild_most_recent_git_tag/unix.after_install.sh
+++ ports/numpy/__cbuild_most_recent_git_tag/unix.after_install.sh
@@ -1,7 +1,8 @@
-if command -v -q brew
-then
-  mkdir -p ~/.local/lib/python${PYTHONVERSION}/site-packages/numpy/.dylibs
-  cp ${INSTALL_PREFIX}/lib/libopenblas* ~/.local/lib/python${PYTHONVERSION}/site-packages/numpy/.dylibs
+if [[ "${BUILD_CONTEXT_macos:=0}" == "1" ]]; then
+  # mkdir -p ~/.local/lib/python${PYTHONVERSION}/site-packages/numpy/.dylibs
+  # cp ${INSTALL_PREFIX}/lib/libopenblas* ~/.local/lib/python${PYTHONVERSION}/site-packages/numpy/.dylibs
+sudo mkdir -p /usr/local/lib
+sudo   cp ${INSTALL_PREFIX}/lib/libopenblas* /usr/local/lib
 fi
 if [[ "${test_numpy}" == "1" ]]; then
   cd ~
