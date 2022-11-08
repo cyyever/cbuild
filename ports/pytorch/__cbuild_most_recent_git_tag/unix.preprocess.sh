5e4400c33dc4bd7f0f2bc486f7b4d1e755c5edcc
cyy <cyyever@outlook.com>
2022-11-08 15:21:45 +0800

    fix code

--- ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++ ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
@@ -2,8 +2,6 @@ cd ${SRC_DIR}
 ${sed_cmd} -i -e "s/'INTEL_MKL_DIR',/'INTEL_MKL_DIR','USE_MEMORY_SANITIZERS','USE_MSAN','USE_TSAN','NO_VPTR','USE_MKLDNN',/" tools/setup_helpers/cmake.py
 ${sed_cmd} -i -e '/^\s*check_submodules()/s/check_submodules()/#check_submodules()/g' setup.py
 ${sed_cmd} -i -e '/Werror/d' third_party/fbgemm/CMakeLists.txt
-${sed_cmd} -i -e '/[^:]move/s/move/std::move/g'  third_party/ideep/mkl-dnn/src/utils/pm/pbuilder.cpp
-${sed_cmd} -i -e 's/std::std::/std::/g'  third_party/ideep/mkl-dnn/src/utils/pm/pbuilder.cpp
 ${sed_cmd} -i -e 's/90/11/g' third_party/foxi/CMakeLists.txt
 # ${sed_cmd} -i -e 's/CMAKE_CXX_STANDARD 17/CMAKE_CXX_STANDARD 23/g' CMakeLists.txt
 if [[ "$(uname)" == "FreeBSD" ]]; then
