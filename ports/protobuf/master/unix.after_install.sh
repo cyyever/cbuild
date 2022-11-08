6eabea5f4dfe0aa1578ab5558f781b29dd8f9aff
cyy <cyyever@outlook.com>
2022-11-08 16:21:01 +0800

    fix port

--- ports/protobuf/master/unix.after_install.sh
+++ ports/protobuf/master/unix.after_install.sh
@@ -1,10 +1,10 @@
+if [[ "${BUILD_CONTEXT_macos:=0}" == "0" ]]; then
 cd ${SRC_DIR}/python
 rm -rf protobuf.egg-info
 rm -rf build
 ${CBUILD_PYTHON_EXE} setup.py build_ext --inplace
 ${CBUILD_PYTHON_EXE} setup.py install --user --force
 
-if [[ "${BUILD_CONTEXT_macos:=0}" == "1" ]]; then
-sudo mkdir -p /usr/local/lib
-sudo   cp ${INSTALL_PREFIX}/lib/libproto* /usr/local/lib
+# sudo mkdir -p /usr/local/lib
+# sudo   cp ${INSTALL_PREFIX}/lib/libproto* /usr/local/lib
 fi
