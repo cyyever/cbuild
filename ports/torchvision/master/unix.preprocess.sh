38e7de7e013c99af98b324b7ca3594cca7e2e066
cyy <cyyever@outlook.com>
2022-12-16 17:26:30 +0800

    use c++17

--- ports/torchvision/master/unix.preprocess.sh
+++ ports/torchvision/master/unix.preprocess.sh
@@ -1,6 +1,6 @@
 cd ${SRC_DIR}
 # ${sed_cmd} -i -e 's/image_link_flags.append("jpeg")/image_link_flags.append("turbojpeg")/g' setup.py
-# ${sed_cmd} -i -e "s#c++14#c++20#g" setup.py
+${sed_cmd} -i -e "s#c++14#c++17#g" setup.py
 ${sed_cmd} -i -e "s/install_requires=requirements/install_requires=[]/" setup.py
 ${sed_cmd} -i -e "/PILLOW_VERSION = /d" test/*py
 ${sed_cmd} -i -e "/affine_quantizer.h/d" torchvision/csrc/ops/quantized/cpu/qnms_kernel.cpp
