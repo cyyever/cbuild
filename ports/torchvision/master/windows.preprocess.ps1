6707493e97a3275583bb3c4fca5a5a32ad7f067c
cyy <cyyever@outlook.com>
2023-01-30 22:16:31 +0800

    fix port

--- ports/torchvision/master/windows.preprocess.ps1
+++ ports/torchvision/master/windows.preprocess.ps1
@@ -1,7 +1,6 @@
 cd $env:SRC_DIR
 # sed_cmd -i -e "/image_link_flags/s/jpeg/turbojpeg/g" setup.py
 sed_cmd -i -e "/image_link_flags/s/libpng/libpng16/g" setup.py
-sed_cmd -i -e "s/install_requires=requirements/install_requires=[]/" setup.py
 sed_cmd -i -e "s/if has_ffmpeg/if False/g" setup.py
 sed_cmd -i -e "/PILLOW_VERSION = /d" test/*py
 sed_cmd -i -e "s/if PILLOW_VERSION >[^)]*)/if True/" test/*py
