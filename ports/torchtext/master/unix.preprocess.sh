7e67bc2fbea8de4c0e29fcdb5544cc4ce2309284
cyy <cyyever@outlook.com>
2023-01-30 22:07:31 +0800

    fix port

--- ports/torchtext/master/unix.preprocess.sh
+++ ports/torchtext/master/unix.preprocess.sh
@@ -4,5 +4,4 @@ ${sed_cmd} -e 's/constexpr unicode_script/unicode_script/' -i third_party/senten
 ${sed_cmd} -e '/DTORCH_COMPILED_WITH_CXX_ABI/d' -i tools/setup_helpers/extension.py
 ${sed_cmd} -e 's/GLIBCXX_USE_CXX11_ABI=0/GLIBCXX_USE_CXX11_ABI=1/' -i CMakeLists.txt
 ${sed_cmd} -e 's/GLIBCXX_USE_CXX11_ABI=0/GLIBCXX_USE_CXX11_ABI=1/' -i third_party/sentencepiece/src/CMakeLists.txt
-${sed_cmd} -i -e 's/CMAKE_CXX_STANDARD 14/CMAKE_CXX_STANDARD 17/g' CMakeLists.txt
 rm -rf build
