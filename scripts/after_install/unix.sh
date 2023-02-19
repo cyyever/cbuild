c8fe69ecff2886b630c37381f552e4204afcadcb
cyy <cyyever@outlook.com>
2023-02-19 21:04:49 +0800

    disable test

--- scripts/after_install/unix.sh
+++ scripts/after_install/unix.sh
@@ -8,8 +8,12 @@ do_static_analysis() {
   if [[ "$json_path" != "" ]]; then
     static_analysis_json_path="$json_path"
     if command -v jq; then
-      jq 'del(.[] | select(.file | contains("/third_party/")))' "$json_path" >"${static_analysis_json_path}.bak"
-      mv "${static_analysis_json_path}.bak" ${static_analysis_json_path}
+      jq 'del(.[] | select(.file | contains("/third_party/")))' "${static_analysis_json_path}" \
+        | jq 'del(.[] | select(.file | contains("/generated/")))' \
+        | jq 'del(.[] | select(.file | contains("/proto/")))' \
+        | jq 'del(.[] | select(.file | contains("/site-packages/")))' \
+        >"${static_analysis_json_path}.bak"
+              mv "${static_analysis_json_path}.bak" ${static_analysis_json_path}
     fi
     if command -v cppcheck; then
       cppcheck --project=$static_analysis_json_path -j $MAX_JOBS --std=c++20 --enable=all --inconclusive 2>${STATIC_ANALYSIS_DIR}/cppcheck.txt || true
@@ -22,10 +26,6 @@ do_static_analysis() {
         plog-converter -t tasklist -a $checking_option -o ${STATIC_ANALYSIS_DIR}/pvs-studio-report.txt ./pvs-studio.log || true
         rm -rf ./pvs-studio.log || true
         if test -f ${STATIC_ANALYSIS_DIR}/pvs-studio-report.txt; then
-          ${sed_cmd} -e '/\/third_party\//d' -i ${STATIC_ANALYSIS_DIR}/pvs-studio-report.txt || true
-          ${sed_cmd} -e '/\/site-packages\//d' -i ${STATIC_ANALYSIS_DIR}/pvs-studio-report.txt || true
-          ${sed_cmd} -e '/\/proto\//d' -i ${STATIC_ANALYSIS_DIR}/pvs-studio-report.txt || true
-          ${sed_cmd} -e '/\/generated\//d' -i ${STATIC_ANALYSIS_DIR}/pvs-studio-report.txt || true
           for error_type in '2005' '103' '106' '112' '108' '107' '104' '2004' '110' '2008' '002' '011' '126' '122'; do
             ${sed_cmd} -e "/\<V${error_type}\>/d" -i ${STATIC_ANALYSIS_DIR}/pvs-studio-report.txt || true
           done
