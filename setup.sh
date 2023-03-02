647dfe756405f32f12a892c61b9e179f227f64d8
cyy <cyyevwer@outlook.com>
2023-03-02 05:25:29 +0000

    fix code

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ if [[ $EUID -eq 0 ]]; then
   sudo_cmd=''
 fi
 
-export python3_cmd=python3
 if command -v apt-get >/dev/null; then
   ${sudo_cmd} apt-get update
   ${sudo_cmd} apt-get install lsb-release jq python3-pip -y
@@ -20,16 +19,22 @@ elif [[ "$(uname -s)" == "Darwin" ]]; then
   brew_cmd=/opt/homebrew/bin/brew
   if ! command -v $brew_cmd >/dev/null; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  $brew_cmd install git
-  $brew_cmd install python3
-  $brew_cmd install jq
-  export python3_cmd=/opt/homebrew/bin/python3
+    $brew_cmd install git
+    $brew_cmd install python3
+    $brew_cmd install jq
+    export python3_cmd=/opt/homebrew/bin/python3
   fi
+fi
+if [[ -z ${python3_cmd+x} ]]; then
+  export python3_cmd=python3
+fi
+if test -f ~/opt/python/bin/python3; then
+  export python3_cmd=$HOME/opt/python/bin/python3
 fi
 
 ${python3_cmd} -m ensurepip --upgrade --user || true
 ${python3_cmd} -m pip install --upgrade pip --user
-${python3_cmd} -m pip install --upgrade --user -r requirements.txt
+${python3_cmd} -m pip install --upgrade --user -r requirements.txt --force
 git pull
 if test -d private_ports; then
   cd private_ports
