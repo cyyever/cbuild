f3db959baf4e36da2afcc858cb7bef74d82ccc48
cyy <cyyever@outlook.com>
2023-03-06 11:34:05 +0800

    fix port

--- ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++ ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
@@ -1,5 +1,5 @@
 cd ${SRC_DIR}
-${sed_cmd} -i -e "/INTEL_MKL_DIR/s/,/,'USE_MKLDNN','USE_NCCL',/" tools/setup_helpers/cmake.py
+${sed_cmd} -i -e "/INTEL_MKL_DIR/s/,/,'USE_MKLDNN','CAFFE2_USE_MKL','USE_NCCL',/" tools/setup_helpers/cmake.py
 ${sed_cmd} -i -e '/^\s*check_submodules()/s/check_submodules()/#check_submodules()/g' setup.py
 ${sed_cmd} -i -e '/finalAtom/s/, pos . startSearchFrom//g' aten/src/ATen/core/qualified_name.h
 if [[ "$(uname)" == "FreeBSD" ]]; then
