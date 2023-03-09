52b6af4217d84c50fc470c39fd5bb3f0e38b8cfd
cyy <cyyever@outlook.com>
2023-03-10 00:59:28 +0800

    fix ports

--- ports/cudnn/master/linux.sh
+++ ports/cudnn/master/linux.sh
@@ -1,11 +1,11 @@
-if command -v pacman
-then
-  rm -rf $INSTALL_PREFIX/cudnn
-  mkdir -p $INSTALL_PREFIX/cudnn/include
-  cp -f -r /usr/include/cudnn* $INSTALL_PREFIX/cudnn/include
-  mkdir -p $INSTALL_PREFIX/cudnn/lib
-  cp -f -r /usr/lib/libcudnn* $INSTALL_PREFIX/cudnn/lib
-else
+# if command -v pacman
+# then
+#   rm -rf $INSTALL_PREFIX/cudnn
+#   mkdir -p $INSTALL_PREFIX/cudnn/include
+#   cp -f -r /usr/include/cudnn* $INSTALL_PREFIX/cudnn/include
+#   mkdir -p $INSTALL_PREFIX/cudnn/lib
+#   cp -f -r /usr/lib/libcudnn* $INSTALL_PREFIX/cudnn/lib
+# else
   cd $SRC_DIR
   cd cudnn*
   rm -rf $INSTALL_PREFIX/cudnn
@@ -13,4 +13,4 @@ else
   cp -f -r include $INSTALL_PREFIX/cudnn
   cp -f -r lib $INSTALL_PREFIX/cudnn
   rm -rf *
-fi
+# fi
