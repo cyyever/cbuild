993309dbda3f7c3d50015383bfe7799780eff79c
cyy <cyyever@outlook.com>
2023-07-12 00:24:23 +0800

    fix code

--- setup.ps1
+++ setup.ps1
@@ -1,12 +1,12 @@
-if ((Get-Command "chocolatey.exe" -ErrorAction SilentlyContinue) -eq $null) {
-    Start-Process -Wait -Verb runAs -FilePath powershell.exe -ArgumentList "Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
+if ((Get-Command "choco.exe" -ErrorAction SilentlyContinue) -eq $null) {
+    Start-Process -Wait -Verb runAs -FilePath powershell.exe -ArgumentList "Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://choco.org/install.ps1'))"
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine"); [System.Environment]::GetEnvironmentVariable("Path", "User")
 }
-Start-Process -Wait -Verb runAs chocolatey.exe -ArgumentList "install -y python --installargs 'Include_debug=1;Include_symbols=1'"
-Start-Process -Wait -Verb runAs chocolatey.exe -ArgumentList "install -y git --params '/GitAndUnixToolsOnPath'"
-# Start-Process -Wait -Verb runAs chocolatey.exe -ArgumentList "install -y pwsh"
-Start-Process -Wait -Verb runAs chocolatey.exe -ArgumentList "install -y gsudo"
-Start-Process -Wait -Verb runAs chocolatey.exe -ArgumentList "install -y vswhere"
+Start-Process -Wait -Verb runAs choco.exe -ArgumentList "install -y python --installargs 'Include_debug=1;Include_symbols=1'"
+Start-Process -Wait -Verb runAs choco.exe -ArgumentList "install -y git --params '/GitAndUnixToolsOnPath'"
+# Start-Process -Wait -Verb runAs choco.exe -ArgumentList "install -y pwsh"
+Start-Process -Wait -Verb runAs choco.exe -ArgumentList "install -y gsudo"
+Start-Process -Wait -Verb runAs choco.exe -ArgumentList "install -y vswhere"
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine"); [System.Environment]::GetEnvironmentVariable("Path", "User")
 python -m pip install --upgrade pip --user
 python -m pip install --upgrade --user -r requirements.txt
