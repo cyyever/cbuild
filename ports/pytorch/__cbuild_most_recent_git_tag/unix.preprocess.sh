89c784b723461fada26f0d70a50a4a0ff959636e
cyy <cyyever@outlook.com>
2023-07-28 10:05:56 +0800

    fix code

--- ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++ ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
@@ -9,9 +9,7 @@ rm -rf ${INSTALL_PREFIX}/include/c10 || true
 rm -rf ${INSTALL_PREFIX}/include/caffe2 || true
 ${sed_cmd} -i -e "/INTEL_MKL_DIR/s/,/,'USE_MKLDNN', 'CUDA_USE_STATIC_CUDA_RUNTIME', 'CAFFE2_USE_MKL','USE_NCCL',/" tools/setup_helpers/cmake.py
 ${sed_cmd} -i -e '/^\s*check_submodules()/s/check_submodules()/#check_submodules()/g' setup.py
-if [[ "${BUILD_CONTEXT_macos:=0}" == "1" ]]; then
-  ${sed_cmd} -i -e '/error=cast-function-type/d' CMakeLists.txt
-fi
+${sed_cmd} -i -e '/error=cast-function-type/d' CMakeLists.txt
 if [[ "$(uname)" == "FreeBSD" ]]; then
   ${sed_cmd} -i -e 's/_assert/assert_in_pytorch/g' aten/src/ATen/native/sparse/ValidateCompressedIndicesCommon.h
   cd third_party
