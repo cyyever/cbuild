3bb4b1ca26f47bc958790d36604f79dd6a081c08
cyy <cyyever@outlook.com>
2023-08-29 09:25:51 +0800

    fix code

--- ports/pybind11/master/windows.after_install.ps1
+++
@@ -1,9 +0,0 @@
-cd $env:SRC_DIR
-Invoke-Expression "$env:CBUILD_PYTHON_EXE setup.py build_ext --inplace"
-if ($LastExitCode -ne 0) {
-  exit $LastExitCode
-}
-Invoke-Expression "$env:CBUILD_PYTHON_EXE setup.py install --user --force"
-if ($LastExitCode -ne 0) {
-  exit $LastExitCode
-}
