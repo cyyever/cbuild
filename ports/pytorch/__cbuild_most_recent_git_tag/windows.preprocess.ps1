568652e0cd58271d4c087f398a40d5f60af19e53
cyy <cyyever@outlook.com>
2023-08-30 09:31:04 +0800

    fix code

--- ports/pytorch/__cbuild_most_recent_git_tag/windows.preprocess.ps1
+++ ports/pytorch/__cbuild_most_recent_git_tag/windows.preprocess.ps1
@@ -1,3 +1,2 @@
-cd "$__SRC_DIR"
-sed_cmd -i -e "/INTEL_MKL_DIR/s/,/,'USE_MKLDNN', 'CUDA_USE_STATIC_CUDA_RUNTIME', 'CAFFE2_USE_MKL','USE_NCCL',/" tools/setup_helpers/cmake.py
+cd "$env:SRC_DIR"
 sed_cmd -i -e '/^\s*check_submodules()/s/check_submodules()/#check_submodules()/g' setup.py
