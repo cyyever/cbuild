33d038c114c3c726216db47010b7d473512e6412
cyy <cyyever@outlook.com>
2023-09-01 10:08:12 +0800

    fix code

--- ports/pytorch/__cbuild_most_recent_git_tag/windows.preprocess.ps1
+++ ports/pytorch/__cbuild_most_recent_git_tag/windows.preprocess.ps1
@@ -1,2 +1,3 @@
 cd "$env:SRC_DIR"
-sed_cmd -i -e '/^\s*check_submodules()/s/check_submodules()/#check_submodules()/g' setup.py
+sed -i -e "/INTEL_MKL_DIR/s/,/,'USE_MKLDNN', 'CUDA_USE_STATIC_CUDA_RUNTIME', 'CAFFE2_USE_MKL','USE_NCCL',/" tools/setup_helpers/cmake.py
+sed -i -e '/^ *check_submodules()/d' setup.py
--- scripts/build/python/windows.ps1
+++ scripts/build/python/windows.ps1
@@ -1,32 +1,33 @@
 cd $__SRC_DIR
 
 if ((Test-Path requirements.txt -PathType Leaf)) {
-  sed_cmd -i -e '/git:/d' requirements.txt
-  Invoke-Expression "$env:CBUILD_PYTHON_EXE install -r requirements.txt --user"
+    sed_cmd -i -e '/git:/d' requirements.txt
+    Invoke-Expression "$env:CBUILD_PYTHON_EXE -m pip install -r requirements.txt --user"
 }
 
 
 if ((Test-Path setup.py -PathType Leaf)) {
-  if ($env:reuse_build -ne "1") {
-    if ((Test-Path build -PathType Container)) {
-      rm -r -Force build
+    if ($env:reuse_build -ne "1") {
+        if ((Test-Path build -PathType Container)) {
+            rm -r -Force build
+        }
+        if ((Test-Path dist -PathType Container)) {
+            rm -r -Force dist
+        }
     }
-    if ((Test-Path dist -PathType Container)) {
-      rm -r -Force dist
+
+    if ((Test-Path env:PYTHON_BUILD_CMD)) {
+        Invoke-Expression "$env:PYTHON_BUILD_CMD"
     }
-  }
+    else {
+        Invoke-Expression "$env:CBUILD_PYTHON_EXE setup.py build_ext --inplace"
+    }
 
-  if ((Test-Path env:PYTHON_BUILD_CMD)) {
-    Invoke-Expression "$env:PYTHON_BUILD_CMD"
-  } else {
-    Invoke-Expression "$env:CBUILD_PYTHON_EXE setup.py build_ext --inplace"
-  }
-
-  if ($LastExitCode -ne 0) {
-    exit $LastExitCode
-  }
-  Invoke-Expression "$env:CBUILD_PYTHON_EXE setup.py install --user --force"
-  if ($LastExitCode -ne 0) {
-    exit $LastExitCode
-  }
+    if ($LastExitCode -ne 0) {
+        exit $LastExitCode
+    }
+    Invoke-Expression "$env:CBUILD_PYTHON_EXE setup.py install --user --force"
+    if ($LastExitCode -ne 0) {
+        exit $LastExitCode
+    }
 }
