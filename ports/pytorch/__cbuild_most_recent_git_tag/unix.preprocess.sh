4be3f9fc528c922ef1de6574a7c7809ce2c39eb4
cyy <cyyever@outlook.com>
2023-09-06 22:20:07 +0800

    fix code

--- ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++ ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
@@ -11,6 +11,8 @@ rm -rf ${INSTALL_PREFIX}/include/sleef.h || true
 ${sed_cmd} -i -e "/INTEL_MKL_DIR/s/,/,'USE_MKLDNN', 'CUDA_USE_STATIC_CUDA_RUNTIME', 'CAFFE2_USE_MKL','USE_NCCL',/" tools/setup_helpers/cmake.py
 ${sed_cmd} -i -e '/^\s*check_submodules()/s/check_submodules()/#check_submodules()/g' setup.py
 ${sed_cmd} -i -e '/error=cast-function-type/d' CMakeLists.txt
+${sed_cmd} -i -e '/CMAKE_CXX_STANDARD/s/17/23/g' CMakeLists.txt
+${sed_cmd} -i -e '/CMAKE_CUDA_STANDARD/s/set(CMAKE_CUDA_STANDARD.*/set(CMAKE_CUDA_STANDARD 17)/g' cmake/public/cuda.cmake
 if [[ "$(uname)" == "FreeBSD" ]]; then
   ${sed_cmd} -i -e 's/_assert/assert_in_pytorch/g' aten/src/ATen/native/sparse/ValidateCompressedIndicesCommon.h
   cd third_party
