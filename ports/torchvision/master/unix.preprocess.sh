fd5c0ca508c9e9f85fd38666d94224ec2395d6db
cyy <cyyever@outlook.com>
2023-09-20 00:14:51 +0800

    fix sed

--- ports/torchvision/master/unix.preprocess.sh
+++ ports/torchvision/master/unix.preprocess.sh
@@ -1,6 +1,4 @@
 cd ${SRC_DIR}
 ${sed_cmd} -i -e "/PILLOW_VERSION = /d" test/*py
 ${sed_cmd} -i -e "s/if PILLOW_VERSION >[^)]*)/if True/" test/*py
-${sed_cmd} -i -e "s/c10::to_string/std::to_string/" torchvision/csrc/io/video/video.cpp
-${sed_cmd} -i -e "s/c10::stoi/std::stoi/" torchvision/csrc/io/video/video.cpp
 rm -rf build
