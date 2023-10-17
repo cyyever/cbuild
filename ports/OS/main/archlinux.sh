699942114489c128da84af7e305a93824b65dd35
cyy <cyyever@outlook.com>
2023-10-17 17:46:56 +0800

    Fix port

--- ports/OS/main/archlinux.sh
+++ ports/OS/main/archlinux.sh
@@ -2,6 +2,10 @@ if [[ "${BUILD_CONTEXT_docker:=0}" == "0" ]]; then
   if systemctl status ldconfig.service; then
     sudo systemctl disable ldconfig.service
   fi
+  if command -v asd; then
+    sudo ${sed_cmd} -i -e "/^#WHATTOSYNC=(/s/.*/WHATTOSYNC=('/home/cyy/.cache/nvim')" /etc/asd.conf
+    sudo systemctl enable asd
+  fi
 elif [[ "${DOCKER_USER:=root}" != "root" ]]; then
   useradd -m -G wheel -s /bin/bash ${DOCKER_USER}
   ${sed_cmd} -i -e '/NOPASSWD/s/#//g' /etc/sudoers
