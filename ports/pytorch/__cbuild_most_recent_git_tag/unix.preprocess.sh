2bd869dac5da827311e97fc817d8cd0f21449d07
cyy <cyyever@outlook.com>
2023-10-22 19:01:26 +0800

    fix code

--- ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++ ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
@@ -14,6 +14,7 @@ ${sed_cmd} -i -e '/^\s*check_submodules()/s/check_submodules()/#check_submodules
 ${sed_cmd} -i -e '/CMAKE_CUDA_STANDARD/s/set(CMAKE_CUDA_STANDARD.*/set(CMAKE_CUDA_STANDARD 17)/g' cmake/public/cuda.cmake
 if [[ "$(uname)" == "FreeBSD" ]]; then
   ${sed_cmd} -i -e 's/_assert/assert_in_pytorch/g' aten/src/ATen/native/sparse/ValidateCompressedIndicesCommon.h
+  ${sed_cmd} -i -e '/defined(__APPLE__)/s/defined(__APPLE__).*/1/g' third_party/onnx/onnx/checker.cc
   cd third_party
   rm -rf cpuinfo
   git clone git@github.com:cyyever/cpuinfo.git
--- ports/torchtext/master/unix.preprocess.sh
+++ ports/torchtext/master/unix.preprocess.sh
@@ -3,4 +3,5 @@ ${sed_cmd} -e '/SPM_ENABLE_TCMALLOC/s/ON/OFF/' -i third_party/sentencepiece/CMak
 ${sed_cmd} -e '/DTORCH_COMPILED_WITH_CXX_ABI/d' -i tools/setup_helpers/extension.py
 ${sed_cmd} -e 's/GLIBCXX_USE_CXX11_ABI=0/GLIBCXX_USE_CXX11_ABI=1/' -i CMakeLists.txt
 ${sed_cmd} -e 's/GLIBCXX_USE_CXX11_ABI=0/GLIBCXX_USE_CXX11_ABI=1/' -i third_party/sentencepiece/src/CMakeLists.txt
+${sed_cmd} -e 's/static_cast<unicode_script::ScriptType>(-1)/static_cast<unicode_script::ScriptType>(0)/' -i third_party/sentencepiece/src/trainer_interface.cc
 rm -rf build
