7ba13d8d1afab342bf00ce23095292dce30295c6
cyy <cyyever@outlook.com>
2023-11-02 12:14:05 +0800

    Fix code

--- scripts/after_install/unix.sh
+++ scripts/after_install/unix.sh
@@ -8,34 +8,34 @@ do_static_analysis() {
   if [[ "$json_path" != "" ]]; then
     static_analysis_json_path="$json_path"
     if command -v jq; then
-      jq 'del(.[] | select(.file | contains("/third_party/")))' "${static_analysis_json_path}" \
-        | jq 'del(.[] | select(.file | contains("/generated/")))' \
-        | jq 'del(.[] | select(.file | contains("/proto/")))' \
-        | jq 'del(.[] | select(.file | contains("/site-packages/")))' \
-        >"${static_analysis_json_path}.bak"
-              mv "${static_analysis_json_path}.bak" ${static_analysis_json_path}
+      jq 'del(.[] | select(.file | contains("/third_party/")))' "${static_analysis_json_path}" |
+        jq 'del(.[] | select(.file | contains("/generated/")))' |
+        jq 'del(.[] | select(.file | contains("/proto/")))' |
+        jq 'del(.[] | select(.file | contains("/site-packages/")))' \
+          >"${static_analysis_json_path}.bak"
+      mv "${static_analysis_json_path}.bak" ${static_analysis_json_path}
     fi
     # if command -v cppcheck; then
     #   cppcheck --project=$static_analysis_json_path -j $MAX_JOBS --std=c++20 --enable=all --inconclusive 2>${STATIC_ANALYSIS_DIR}/cppcheck.txt || true
     # fi
-    if [[ "${slow_static_analysis:=0}" == "1" ]]; then
-      if command -v pvs-studio; then
-        pvs-studio-analyzer analyze --intermodular --file ${static_analysis_json_path} -a 31 -o ./pvs-studio.log -j10 || true
-        checking_option='GA:1,2,3;64:1,2,3;OP:1,2,3;CS:1,2,3'
-        # checking_option='GA:1,2;64:1,2;OP:1,2;CS:1,2'
-        plog-converter -t tasklist -a $checking_option -o ${STATIC_ANALYSIS_DIR}/pvs-studio-report.txt ./pvs-studio.log || true
-        rm -rf ./pvs-studio.log || true
-        if test -f ${STATIC_ANALYSIS_DIR}/pvs-studio-report.txt; then
-          for error_type in '2005' '103' '106' '112' '108' '107' '104' '2004' '110' '2008' '002' '011' '126' '122'; do
-            ${sed_cmd} -e "/\<V${error_type}\>/d" -i ${STATIC_ANALYSIS_DIR}/pvs-studio-report.txt || true
-          done
-          grep -e "${__SRC_DIR}" ${STATIC_ANALYSIS_DIR}/pvs-studio-report.txt | sort -k 4 >pvs.txt && mv pvs.txt ${STATIC_ANALYSIS_DIR}/pvs-studio-report.txt || true
-        fi
+    if command -v pvs-studio; then
+      pvs-studio-analyzer analyze --intermodular --file ${static_analysis_json_path} -a 31 -o ./pvs-studio.log -j10 || true
+      checking_option='GA:1,2,3;64:1,2,3;OP:1,2,3;CS:1,2,3'
+      # checking_option='GA:1,2;64:1,2;OP:1,2;CS:1,2'
+      plog-converter -t tasklist -a $checking_option -o ${STATIC_ANALYSIS_DIR}/pvs-studio-report.txt ./pvs-studio.log || true
+      rm -rf ./pvs-studio.log || true
+      if test -f ${STATIC_ANALYSIS_DIR}/pvs-studio-report.txt; then
+        for error_type in '2005' '103' '106' '112' '108' '107' '104' '2004' '110' '2008' '002' '011' '126' '122'; do
+          ${sed_cmd} -e "/\<V${error_type}\>/d" -i ${STATIC_ANALYSIS_DIR}/pvs-studio-report.txt || true
+        done
+        grep -e "${__SRC_DIR}" ${STATIC_ANALYSIS_DIR}/pvs-studio-report.txt | sort -k 4 >pvs.txt && mv pvs.txt ${STATIC_ANALYSIS_DIR}/pvs-studio-report.txt || true
       fi
-      get_run_clang_tidy_cmd
-      if [[ "${run_clang_tidy_cmd}" != "" ]]; then
-        cd $__SRC_DIR
-        eval "${run_clang_tidy_cmd} -j $MAX_JOBS -p $(dirname $json_path) -quiet >${STATIC_ANALYSIS_DIR}/run-clang-tidy.txt || true"
+      if [[ "${slow_static_analysis:=0}" == "1" ]]; then
+        get_run_clang_tidy_cmd
+        if [[ "${run_clang_tidy_cmd}" != "" ]]; then
+          cd $__SRC_DIR
+          eval "${run_clang_tidy_cmd} -j $MAX_JOBS -p $(dirname $json_path) -quiet >${STATIC_ANALYSIS_DIR}/run-clang-tidy.txt || true"
+        fi
       fi
     fi
   fi
