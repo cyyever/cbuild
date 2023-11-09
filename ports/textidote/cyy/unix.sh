4cb688b67ea2e1241774fc3a5f476dea2a8c2616
cyy <cyyever@outlook.com>
2023-11-09 10:29:07 +0800

    Fix port

--- ports/textidote/cyy/unix.sh
+++ ports/textidote/cyy/unix.sh
@@ -2,4 +2,4 @@ cd ${SRC_DIR}
 ant download-deps
 ant
 mkdir -p $INSTALL_PREFIX/textidote
-cp -r textidote.jar $INSTALL_PREFIX/textidote
+cp -r textidote-0.9.jar $INSTALL_PREFIX/textidote/textidote.jar
