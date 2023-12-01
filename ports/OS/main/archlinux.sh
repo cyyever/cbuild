c88fbc0e794eea7d88530390673d682904ebfcca
cyy <cyyever@outlook.com>
2023-12-01 09:13:47 +0800

    Fix code

--- ports/OS/main/archlinux.sh
+++ ports/OS/main/archlinux.sh
@@ -6,6 +6,9 @@ if [[ "${BUILD_CONTEXT_docker:=0}" == "0" ]]; then
     sudo systemctl enable asd
   fi
 elif [[ "${DOCKER_USER:=root}" != "root" ]]; then
+  rm -rf /usr/lib/libcudnn*static*
+  rm /opt/cuda/targets/x86_64-linux/lib/libcublas_static.a
+  rm /opt/cuda/targets/x86_64-linux/lib/libcublasLt_static.a
   useradd -m -G wheel -s /bin/bash ${DOCKER_USER}
   ${sed_cmd} -i -e '/NOPASSWD/s/#//g' /etc/sudoers
 fi
