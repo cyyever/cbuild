f289a3c6788686f7f9c3e418c0616d0f5eeb855d
cyy <cyyever@outlook.com>
2023-12-02 17:32:15 +0800

    Fix port

--- ports/OS/main/archlinux.sh
+++ ports/OS/main/archlinux.sh
@@ -5,10 +5,13 @@ if [[ "${BUILD_CONTEXT_docker:=0}" == "0" ]]; then
   if command -v asd; then
     sudo systemctl enable asd
   fi
-elif [[ "${DOCKER_USER:=root}" != "root" ]]; then
+else
   rm -rf /usr/lib/libcudnn*static*
-  rm /opt/cuda/targets/x86_64-linux/lib/libcublas_static.a
   rm /opt/cuda/targets/x86_64-linux/lib/libcublasLt_static.a
-  useradd -m -G wheel -s /bin/bash ${DOCKER_USER}
-  ${sed_cmd} -i -e '/NOPASSWD/s/#//g' /etc/sudoers
+  rm /opt/cuda/targets/x86_64-linux/lib/libcusparse_static.a
+  rm /opt/cuda/targets/x86_64-linux/lib/libcufft_static*
+  if [[ "${DOCKER_USER:=root}" != "root" ]]; then
+    useradd -m -G wheel -s /bin/bash ${DOCKER_USER}
+    ${sed_cmd} -i -e '/NOPASSWD/s/#//g' /etc/sudoers
+  fi
 fi
