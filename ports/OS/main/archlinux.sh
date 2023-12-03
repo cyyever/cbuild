b175864c697e67d7794bdeacbdceb90296e57315
cyy <cyyever@outlook.com>
2023-12-03 12:16:21 +0800

    Fix code

--- ports/OS/main/archlinux.sh
+++ ports/OS/main/archlinux.sh
@@ -10,6 +10,10 @@ else
   rm /opt/cuda/targets/x86_64-linux/lib/libcublasLt_static.a
   rm /opt/cuda/targets/x86_64-linux/lib/libcusparse_static.a
   rm /opt/cuda/targets/x86_64-linux/lib/libcufft_static*
+  rm /opt/cuda/targets/x86_64-linux/lib/libcurand_static.a
+  rm /opt/cuda/targets/x86_64-linux/lib/libnppif_static.a
+  rm /opt/cuda/targets/x86_64-linux/lib/libcusolver_static.a
+  rm /opt/cuda/targets/x86_64-linux/lib/libcublas_static.a
   if [[ "${DOCKER_USER:=root}" != "root" ]]; then
     useradd -m -G wheel -s /bin/bash ${DOCKER_USER}
     ${sed_cmd} -i -e '/NOPASSWD/s/#//g' /etc/sudoers
