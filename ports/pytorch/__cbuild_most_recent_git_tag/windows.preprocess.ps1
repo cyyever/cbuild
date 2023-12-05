1ae58d99ef1ccef411ae07627a51a058e259eebb
cyy <cyyever@outlook.com>
2023-12-05 17:02:22 +0800

    Fix code

--- ports/pytorch/__cbuild_most_recent_git_tag/windows.preprocess.ps1
+++ ports/pytorch/__cbuild_most_recent_git_tag/windows.preprocess.ps1
@@ -1,5 +1,4 @@
 cd "$env:SRC_DIR"
 sed -i -e "/INTEL_MKL_DIR/s/,/,'USE_MKLDNN', 'CUDA_USE_STATIC_CUDA_RUNTIME', 'CAFFE2_USE_MKL','USE_NCCL',/" tools/setup_helpers/cmake.py
 sed -i -e '/^ *check_submodules()/d' setup.py
-sed -i -e '/Dependencies.cmake/d' CMakeLists.txt
 rm  cmake/Modules/FindMKL.cmake
