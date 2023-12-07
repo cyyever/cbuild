8c0b2f7b3c1f1987c870e23dd6fdf75b62b446db
cyy <cyyever@outlook.com>
2023-12-07 10:11:42 +0800

    Fix port

--- ports/cpython/master/unix.after_install.sh
+++ ports/cpython/master/unix.after_install.sh
@@ -1,7 +1,7 @@
 if [[ "${BUILD_CONTEXT_docker:=0}" == 1 ]]; then
   ${CBUILD_PYTHON_EXE} -c "import os"
-  echo "export PATH=${PATH}" >>~/.profile
-  echo "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}" >>~/.profile
-  echo "export LIBRARY_PATH=${LIBRARY_PATH}" >>~/.profile
-  echo "export PYTHONPATH=${PYTHONPATH}" >>~/.profile
+  echo "export PATH=${PATH}" >>~/.bashrc
+  echo "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}" >>~/.bashrc
+  echo "export LIBRARY_PATH=${LIBRARY_PATH}" >>~/.bashrc
+  echo "export PYTHONPATH=${PYTHONPATH}" >>~/.bashrc
 fi
