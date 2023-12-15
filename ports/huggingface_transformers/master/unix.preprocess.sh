3ce4c5f12248331cfbed469533258bc219ad9aa7
cyy <cyyever@outlook.com>
2023-12-15 10:52:29 +0800

    Fix code

--- ports/huggingface_transformers/master/unix.preprocess.sh
+++ ports/huggingface_transformers/master/unix.preprocess.sh
@@ -1,3 +1,3 @@
 cd ${SRC_DIR}
 ${sed_cmd} -i -e "/fsspec/d" setup.py
-# ${sed_cmd} -i -e "s/_register_pytree_node/register_pytree_node/g" src/transformers/utils/generic.py
+${sed_cmd} -i -e "s/_register_pytree_node/register_pytree_node/g" src/transformers/utils/generic.py
