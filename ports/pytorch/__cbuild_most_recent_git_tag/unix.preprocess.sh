8220c793d6017604854a47e5734e07fb177f4c31
cyy <cyyever@outlook.com>
2023-12-21 13:21:05 +0800

    Fix script

--- ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++ ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
@@ -8,6 +8,7 @@ rm -rf ${INSTALL_PREFIX}/include/ATen || true
 rm -rf ${INSTALL_PREFIX}/include/c10 || true
 rm -rf ${INSTALL_PREFIX}/include/caffe2 || true
 rm -rf ${INSTALL_PREFIX}/include/sleef.h || true
+rm -rf ${INSTALL_PREFIX}/include/xnnpack.h || true
 ${sed_cmd} -i -e "/INTEL_MKL_DIR/s/,/,'USE_MKLDNN', 'CUDA_USE_STATIC_CUDA_RUNTIME', 'CAFFE2_USE_MKL','USE_NCCL',/" tools/setup_helpers/cmake.py
 ${sed_cmd} -i -e '/^\s*check_submodules()/s/check_submodules()/#check_submodules()/g' setup.py
 # ${sed_cmd} -i -e '/CMAKE_CXX_STANDARD/s/17/23/g' CMakeLists.txt
