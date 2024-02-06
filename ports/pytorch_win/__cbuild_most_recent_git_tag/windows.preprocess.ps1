bc54d9f3c36ee4c2dbf10bada0e4f0d66da0c8a2
cyy <cyyever@outlook.com>
2024-02-06 11:32:54 +0800

    Fix port

---
+++ ports/pytorch_win/__cbuild_most_recent_git_tag/windows.preprocess.ps1
@@ -0,0 +1,7 @@
+cd "$env:SRC_DIR"
+sed -i -e "/INTEL_MKL_DIR/s/,/,'USE_MKLDNN', 'CUDA_USE_STATIC_CUDA_RUNTIME', 'CAFFE2_USE_MKL','USE_NCCL',/" tools/setup_helpers/cmake.py
+sed -i -e '/^ *check_submodules()/d' setup.py
+rm  cmake/Modules/FindMKL.cmake
+sed -i -e "/MKL_INCLUDE_DIR/s/MKL_INCLUDE_DIR/MKL_INCLUDE/g"  cmake/public/mkl.cmake
+sed -i -e "/target_link_libraries/s/..MKL_LIBRARIES./MKL::MKL/g"  cmake/public/mkl.cmake
+sed -i -e "/MKL_INCLUDE_DIR/s/MKL_INCLUDE_DIR/MKL_INCLUDE/g"  cmake/Dependencies.cmake
