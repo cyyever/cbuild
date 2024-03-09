d1c329e1200d592ce03e391eacab1306c29075f0
cyy <cyyever@outlook.com>
2024-03-09 12:08:01 +0800

    Use C++23

--- ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++ ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
@@ -11,7 +11,7 @@ rm -rf ${INSTALL_PREFIX}/include/sleef.h || true
 rm -rf ${INSTALL_PREFIX}/include/xnnpack.h || true
 ${sed_cmd} -i -e "/INTEL_MKL_DIR/s/,/,'USE_MKLDNN', 'CUDA_USE_STATIC_CUDA_RUNTIME', 'USE_STATIC_MKL', 'CAFFE2_USE_MKL','USE_NCCL',/" tools/setup_helpers/cmake.py
 ${sed_cmd} -i -e '/^\s*check_submodules()/s/check_submodules()/#check_submodules()/g' setup.py
-${sed_cmd} -i -e '/CMAKE_CXX_STANDARD/s/17/20/g' CMakeLists.txt
+${sed_cmd} -i -e '/CMAKE_CXX_STANDARD/s/17/23/g' CMakeLists.txt
 ${sed_cmd} -i -e '/CMAKE_CUDA_STANDARD/s/set(CMAKE_CUDA_STANDARD.*/set(CMAKE_CUDA_STANDARD 17)/g' cmake/public/cuda.cmake
 ${sed_cmd} -i -e '/int64_t max_split_size/s/int64_t/size_t/g' c10/cuda/CUDACachingAllocator.h
 ${sed_cmd} -i -e '/CUDAHooks.cpp PROPERTIES INCLU/d' caffe2/CMakeLists.txt
