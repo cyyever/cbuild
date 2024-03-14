f81747b56d17d25af9f54b067deaba9aaf09b8d3
cyy <cyyever@outlook.com>
2024-03-14 10:24:05 +0800

    Remove unused ports

--- ports/torchaudio/master/unix.preprocess.sh
+++
@@ -1,3 +0,0 @@
-cd ${SRC_DIR}
-${sed_cmd} -i -e "/torch>=/d" requirements.txt
-${sed_cmd} -i -e "s/list(APPEND TORCHAUDIO_THIRD_PARTIES libsox)/list(APPEND TORCHAUDIO_THIRD_PARTIES libsox gsm)/" third_party/CMakeLists.txt
