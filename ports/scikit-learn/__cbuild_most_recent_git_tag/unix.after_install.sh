78370ad8f7dd8a0ae2c3214a30c7b0b44d749f03
cyy <cyyever@outlook.com>
2024-03-19 16:09:16 +0800

    Fix formatting

--- ports/scikit-learn/__cbuild_most_recent_git_tag/unix.after_install.sh
+++ ports/scikit-learn/__cbuild_most_recent_git_tag/unix.after_install.sh
@@ -1,8 +1,8 @@
 if [[ "${BUILD_CONTEXT_macos:=0}" == "1" ]]; then
   # mkdir -p ~/.local/lib/python${PYTHONVERSION}/site-packages/numpy/.dylibs
   # cp ${INSTALL_PREFIX}/lib/libopenblas* ~/.local/lib/python${PYTHONVERSION}/site-packages/numpy/.dylibs
-sudo mkdir -p /usr/local/lib
-sudo   cp ${INSTALL_PREFIX}/lib/libopenblas* /usr/local/lib
+  sudo mkdir -p /usr/local/lib
+  sudo cp ${INSTALL_PREFIX}/lib/libopenblas* /usr/local/lib
 fi
 if [[ "${test_numpy}" == "1" ]]; then
   cd ~
