b1125723db152c9baa613dfcab6c67aacf624b43
cyy <cyyever@outlook.com>
2024-03-29 12:08:24 +0800

    Fix script

--- scripts/package_manager/apt.sh
+++ scripts/package_manager/apt.sh
@@ -4,20 +4,24 @@ fi
 if ! test -t 0; then
   export DEBIAN_FRONTEND=noninteractive
 fi
+has_pkg_installed=0
 for pkg in ${apt_pkgs}; do
   if ! dpkg-query --show --showformat='${db:Status-Status}' $pkg | grep -i -E '^installed' >/dev/null; then
-    if [[ "${no_apt_update:=1}" == 1 ]]; then
+    if [[ "${no_apt_update:=1}" == 0 ]]; then
       ${sudo_cmd} apt-get update
-      no_apt_update=0
+      no_apt_update=1
     fi
     ${sudo_cmd} apt-get install --no-install-recommends -y $pkg
+    has_pkg_installed=1
   fi
 done
 
 if [[ "${BUILD_CONTEXT_docker:=0}" == 1 ]]; then
-  ${sudo_cmd} rm -rf /var/lib/apt/lists/*
-  ${sudo_cmd} rm -rf /usr/local/share/doc
-  ${sudo_cmd} rm -rf /usr/local/share/man
-  ${sudo_cmd} apt-get clean -y
-  ${sudo_cmd} apt-get purge -y
+  if [[ "${has_pkg_installed}" == 1 ]]; then
+    ${sudo_cmd} rm -rf /var/lib/apt/lists/*
+    ${sudo_cmd} rm -rf /usr/local/share/doc
+    ${sudo_cmd} rm -rf /usr/local/share/man
+    ${sudo_cmd} apt-get clean -y
+    ${sudo_cmd} apt-get purge -y
+  fi
 fi
