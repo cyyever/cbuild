306b268b75dd4e43ca4c03a45ace10b164f0e1f5
cyy <cyyever@outlook.com>
2024-04-12 22:32:52 +0800

    Fix code

--- ports/pytorch/__cbuild_most_recent_git_tag/windows.preprocess.ps1
+++ ports/pytorch/__cbuild_most_recent_git_tag/windows.preprocess.ps1
@@ -1,5 +1,6 @@
 cd "$env:SRC_DIR"
 git remote add up git@github.com:cyyever/pytorch.git
+git fetch --all
 git rebase up/nvtx3_fix2
 rm -r -ErrorAction SilentlyContinue $env:INSTALL_PREFIX/lib/libc10*
 rm -r -ErrorAction SilentlyContinue $env:INSTALL_PREFIX/lib/libtorch*
