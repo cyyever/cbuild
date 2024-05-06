cb5553df61afc135bf137e7259e84b67caf0d45d
cyy <cyyever@outlook.com>
2024-05-06 09:35:02 +0800

    Add script

--- ports/pybind11/master/unix.preprocess.sh
+++ ports/pybind11/master/unix.preprocess.sh
@@ -1,5 +1,7 @@
 cd ${SRC_DIR}
 if [[ "$(uname)" == "FreeBSD" ]]; then
   # ${sed_cmd} -i -e 's/if defined(__apple_build_version__)/if defined(__apple_build_version__) || defined(_LIBCPP_VERSION)/g' include/pybind11/detail/common.h
+  ${sed_cmd} -i -e 's/, .ninja.//g' pyproject.toml
+  ${sed_cmd} -i -e 's/, .cmake.*"//g' pyproject.toml
   ${sed_cmd} -i -e 's/if defined(__apple_build_version__)/if 1/g' include/pybind11/detail/common.h
 fi
