c06f4ed5e6e0132901d4897d25f1a1582812f7c2
cyy <cyyever@outlook.com>
2024-05-20 08:00:37 +0800

    Fix port

--- ports/apache_arrow_cpp/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++
@@ -1,4 +0,0 @@
-cd ${SRC_DIR}
-if [[ "$(uname)" == "FreeBSD" ]]; then
-  ${sed_cmd} -i -e 's/AMD64/amd64/g' cpp/cmake_modules/SetupCxxFlags.cmake
-fi
--- ports/apache_arrow_python/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++
@@ -1,5 +0,0 @@
-cd ${SRC_DIR}
-if [[ "$(uname)" == "FreeBSD" ]]; then
-  ${sed_cmd} -i -e 's/AMD64/amd64/g' cpp/cmake_modules/SetupCxxFlags.cmake
-fi
-${sed_cmd} -i -e '/Cython.__version__/s/.*/if False:/g' python/setup.py
