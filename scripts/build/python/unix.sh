9e5bd45c151f46de5cf8be7be52bd7bf346ed111
cyy <cyyever@outlook.com>
2024-05-21 23:24:54 +0800

    Fix port

--- scripts/build/python/unix.sh
+++ scripts/build/python/unix.sh
@@ -10,6 +10,7 @@ if test -f requirements.txt; then
   ${sed_cmd} -i -e '/torch/d' requirements.txt
   ${sed_cmd} -i -e '/nvidia/d' requirements.txt
   ${sed_cmd} -i -e '/numpy/d' requirements.txt
+  ${sed_cmd} -i -e '/cython/d' requirements.txt
   ${CBUILD_PIP_EXE} install -r requirements.txt --user
 fi
 
