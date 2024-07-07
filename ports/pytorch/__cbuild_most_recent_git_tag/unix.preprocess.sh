695f27fbe4e82c6cf7ab4c258d2d9b6336723e1c
cyy <cyyever@outlook.com>
2024-07-07 16:03:39 +0800

    Fix config

--- ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++ ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
@@ -13,9 +13,10 @@ ${sed_cmd} -i -e "/INTEL_MKL_DIR/s/,/,'USE_MKLDNN', 'CUDA_USE_STATIC_CUDA_RUNTIM
 ${sed_cmd} -i -e '/^\s*check_submodules()/s/check_submodules()/#check_submodules()/g' setup.py
 ${sed_cmd} -i -e '/CMAKE_CUDA_STANDARD/s/set(CMAKE_CUDA_STANDARD.*/set(CMAKE_CUDA_STANDARD 17)/g' cmake/public/cuda.cmake
 ${sed_cmd} -i -e '/int64_t max_split_size/s/int64_t/size_t/g' c10/cuda/CUDACachingAllocator.h
-# if [[ "${BUILD_CONTEXT_macos:=0}" != "1" ]]; then
-#   ${sed_cmd} -i -e '/CMAKE_CXX_STANDARD/s/17/23/g' CMakeLists.txt
-# fi
+if [[ "${BUILD_CONTEXT_macos:=0}" != "1" ]]; then
+  ${sed_cmd} -i -e '/CMAKE_CXX_STANDARD/s/17/20/g' CMakeLists.txt
+fi
+${sed_cmd} -i -e '/Werror/d' third_party/fbgemm/CMakeLists.txt
 
 if [[ "$(uname)" == "FreeBSD" ]]; then
   ${sed_cmd} -i -e 's/_assert/assert_in_pytorch/g' aten/src/ATen/native/sparse/ValidateCompressedIndicesCommon.h
