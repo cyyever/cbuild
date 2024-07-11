e886d31b76a3cc017a5248cf4f4185678d4b0dba
cyy <cyyever@outlook.com>
2024-07-11 15:48:44 +0800

    Fix code

--- scripts/package_manager/npm.sh
+++ scripts/package_manager/npm.sh
@@ -1,5 +1,6 @@
 for pkg in ${npm_pkgs}; do
-  npm install --prefix "${INSTALL_PREFIX}" ${pkg}
+  npm install --prefix "${INSTALL_PREFIX}" ${pkg}@latest
 done
 cd $INSTALL_PREFIX
+npm update || true
 npm audit fix || true
