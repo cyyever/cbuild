7de3f0fca6fcd0e43e70be26f68fa0262e401127
cyy <cyyever@outlook.com>
2024-08-02 10:44:15 +0800

    Fix code

--- ports/dali/master/unix.preprocess.sh
+++ ports/dali/master/unix.preprocess.sh
@@ -1,4 +1,6 @@
+export python=${CBUILD_PYTHON_EXE}
 cd ${SRC_DIR}
 rm -rf ${INSTALL_PREFIX}/include/dali
 rm -rf ${INSTALL_PREFIX}/lib/libdali*
 ${sed_cmd} -i -e '/check_and_add_cmake_submodule.*pybind11/d' cmake/Dependencies.common.cmake
+${sed_cmd} -i -e 's/COMMAND python/COMMAND python3/g' $(grep 'COMMAND python'  -r * -l)
