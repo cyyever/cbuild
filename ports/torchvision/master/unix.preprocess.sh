49121706279d939f12631c7ce2ce557e5d60f6eb
cyy <cyyever@outlook.com>
2024-08-08 18:29:55 +0800

    Fix port

--- ports/torchvision/master/unix.preprocess.sh
+++ ports/torchvision/master/unix.preprocess.sh
@@ -2,4 +2,7 @@ cd ${SRC_DIR}
 ${sed_cmd} -e 's/, "torch"//g' -i pyproject.toml
 ${sed_cmd} -e 's/pytorch_dep,//g' -i setup.py
 ${sed_cmd} -e 's/"numpy",//g' -i setup.py
+if [[ "$(uname)" == "FreeBSD" ]]; then
+  ${sed_cmd} -e 's/library = "libpng"/library = "png"/g' -i setup.py
+fi
 rm -rf build
