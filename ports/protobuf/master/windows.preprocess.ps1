e26a991388e1b860db65a0bdb54db930fd84ba21
cyy <cyyever@outlook.com>
2024-08-19 06:25:58 +0800

    Fix port

--- ports/protobuf/master/windows.preprocess.ps1
+++ ports/protobuf/master/windows.preprocess.ps1
@@ -1,4 +1,7 @@
 cd $env:INSTALL_PREFIX
 if ((Test-Path -Path "include/google/protobuf" -PathType Container)) {
-  Remove-Item include/google/protobuf -Recurse -Force
+    Remove-Item include/google/protobuf -Recurse -Force
 }
+cd "$env:SRC_DIR"
+#sed -i -e '/abseil_test_dll/d' cmake/abseil-cpp.cmake
+#sed -i -e '/abseil_test_dll/d' cmake/tests.cmake
