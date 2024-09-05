a0c3b03c4377ad3ff2b498b86df31df1a4a225f0
cyy <cyyever@outlook.com>
2024-09-05 16:24:44 +0800

    Fix golang

--- scripts/build/golang/unix.sh
+++ scripts/build/golang/unix.sh
@@ -1,4 +1,15 @@
-cd ${__SRC_DIR}
-go generate ./...
-go build .
-go install
+if [[ -n ${GO_SRC_DIRS+x} ]]; then
+  for GO_SRC_DIR in $(echo "$GO_SRC_DIRS" | tr ":" "\n"); do
+    cd ${SRC_DIR}
+    cd ${GO_SRC_DIR}
+    pwd
+    go generate ./...
+    go build .
+    go install
+  done
+else
+  cd ${__SRC_DIR}
+  go generate ./...
+  go build .
+  go install
+fi
