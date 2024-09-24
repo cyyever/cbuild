794f6ad16962416b7bc3611aca15e5266209b802
cyy <cyyever@outlook.com>
2024-09-24 16:31:31 +0800

    Supress some warnings

--- scripts/after_install/unix.sh
+++ scripts/after_install/unix.sh
@@ -25,7 +25,7 @@ do_static_analysis() {
       plog-converter -t tasklist -a $checking_option -o ${STATIC_ANALYSIS_DIR}/pvs-studio-report.txt ./pvs-studio.log || true
       rm -rf ./pvs-studio.log || true
       if test -f ${STATIC_ANALYSIS_DIR}/pvs-studio-report.txt; then
-        for error_type in '2005' '103' '106' '112' '108' '107' '104' '2004' '110' '2008' '002' '011' '126' '122'; do
+        for error_type in '831' '837' '2005' '103' '106' '112' '108' '107' '104' '2004' '110' '2008' '002' '011' '126' '122'; do
           ${sed_cmd} -e "/\<V${error_type}\>/d" -i ${STATIC_ANALYSIS_DIR}/pvs-studio-report.txt || true
         done
         grep -e "${__SRC_DIR}" ${STATIC_ANALYSIS_DIR}/pvs-studio-report.txt | sort -k 4 >pvs.txt && mv pvs.txt ${STATIC_ANALYSIS_DIR}/pvs-studio-report.txt || true
