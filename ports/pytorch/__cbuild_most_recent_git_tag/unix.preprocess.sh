7a7bd27571b0e6e9bc998a510c452fd229d264dc
cyy <cyyever@outlook.com>
2024-10-07 09:26:00 +0800

    Fix port

--- ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++ ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
@@ -12,7 +12,6 @@ rm -rf ${INSTALL_PREFIX}/include/xnnpack.h || true
 ${sed_cmd} -i -e "/INTEL_MKL_DIR/s/,/,'USE_MKLDNN', 'CUDA_USE_STATIC_CUDA_RUNTIME', 'USE_NCCL','CMAKE_CXX_STANDARD',/" tools/setup_helpers/cmake.py
 ${sed_cmd} -i -e '/^\s*check_submodules()/s/check_submodules()/#check_submodules()/g' setup.py
 ${sed_cmd} -i -e '/int64_t max_split_size/s/int64_t/size_t/g' c10/cuda/CUDACachingAllocator.h
-${sed_cmd} -i -e '/lintrunner/d' requirements.txt
 ${sed_cmd} -i -e '/ninja/d' requirements.txt
 ${sed_cmd} -i -e '/Werror/d' third_party/fbgemm/CMakeLists.txt
 ${sed_cmd} -i -e '/opentelemetry/d' torch/CMakeLists.txt
@@ -22,7 +21,6 @@ if [[ "$(uname)" == "FreeBSD" ]]; then
   ${sed_cmd} -i -e '/defined(__APPLE__)/s/defined(__APPLE__).*/1/g' third_party/onnx/onnx/checker.cc
 fi
 
-# ${sed_cmd} -i -e '1iINCLUDE(CheckFunctionExists)' cmake/Modules/FindBLAS.cmake
 if [[ "${BUILD_CONTEXT_macos:=0}" == "0" ]]; then
   ${sed_cmd} -i -e '/set(TP_BUILD_LIBUV ON CACHE BOOL "" FORCE)/s/ON/OFF/g' cmake/Dependencies.cmake
 fi
