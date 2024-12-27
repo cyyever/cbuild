a39e109ed9e4822cfe57a776eae9019f877e95a7
cyy <cyyever@outlook.com>
2024-12-27 11:16:47 +0800

    Fix config

--- ports/vllm/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++ ports/vllm/__cbuild_most_recent_git_tag/unix.preprocess.sh
@@ -1,4 +1,5 @@
 cd ${SRC_DIR}
 ${sed_cmd} -i -e '/torch.*== /d' *.toml
 ${sed_cmd} -i -e '/install_requires=get_requirement/d' setup.py
+${sed_cmd} -i -e '/enable_cudnn_sdp/d' vllm/platforms/cuda.py
 rm requirements*
