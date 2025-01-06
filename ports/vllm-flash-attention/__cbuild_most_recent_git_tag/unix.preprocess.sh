fe9abb063b823b13b798958e6398eb552616aea5
cyy <cyyever@outlook.com>
2025-01-06 10:15:57 +0800

    Update port

--- ports/vllm-flash-attention/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++ ports/vllm-flash-attention/__cbuild_most_recent_git_tag/unix.preprocess.sh
@@ -1,9 +1,6 @@
 cd ${SRC_DIR}
 ${sed_cmd} -i -e '/torch.*== /d' *.toml
 ${sed_cmd} -i -e '/install_requires=get_requirement/d' setup.py
-${sed_cmd} -i -e '/enable_cudnn_sdp/d' vllm/platforms/cuda.py
 ${sed_cmd} -i -e '/PYTHON_SUPPORTED_VERSIONS/s/3.9/3.13/g' CMakeLists.txt
-${sed_cmd} -i -e '/PYTHON_SUPPORTED_VERSIONS/s/3.9/3.13/g' .deps/vllm-flash-attn-src/CMakeLists.txt
 ${sed_cmd} -i -e 's/7.0;7.2;7.5;//g' CMakeLists.txt
-
-rm requirements*
+exit 0
