779687ca1e5918cf7434325b77281bd8ac1059f2
cyy <cyyever@outlook.com>
2025-02-01 15:52:22 +0800

    Fix code

--- ports/pytorch/__cbuild_most_recent_git_tag/windows.preprocess.ps1
+++ ports/pytorch/__cbuild_most_recent_git_tag/windows.preprocess.ps1
@@ -9,11 +9,9 @@ rm -r -ErrorAction SilentlyContinue $env:INSTALL_PREFIX/include/c10
 rm -r -ErrorAction SilentlyContinue $env:INSTALL_PREFIX/include/caffe2
 rm -r -ErrorAction SilentlyContinue $env:INSTALL_PREFIX/include/sleef.h
 rm -r -ErrorAction SilentlyContinue $env:INSTALL_PREFIX/include/xnnpack.h
-sed -i -e "/INTEL_MKL_DIR/s/,/,'USE_MKLDNN', 'USE_NCCL',/" tools/setup_helpers/cmake.py
-
+sed -i -e "/INTEL_MKL_DIR/s/,/,'USE_MKLDNN', 'USE_NCCL','CMAKE_CXX_STANDARD','CMAKE_CUDA_STANDARD'/" tools/setup_helpers/cmake.py
 sed -i -e '/^ *check_submodules()/d' setup.py
 rm cmake/Modules/FindMKL.cmake
-sed -i -e '1 i\include(CheckFunctionExists)'  cmake/Modules/FindLAPACK.cmake
 sed -i -e '/pentelemet/d' torch/CMakeLists.txt
 sed -i -e '/my_env = _overlay_windows_vcvars/d' tools/build_pytorch_libs.py
 sed -i -e '/cmake/d' requirements.txt
