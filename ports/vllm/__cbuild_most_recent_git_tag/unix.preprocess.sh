7aad22c1486e12a033b706854c1b1fdce2fde20e
cyy <cyyever@outlook.com>
2025-02-11 16:59:11 +0800

    Revert back to 3.12.9

--- ports/vllm/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++ ports/vllm/__cbuild_most_recent_git_tag/unix.preprocess.sh
@@ -2,6 +2,6 @@ cd ${SRC_DIR}
 ${sed_cmd} -i -e '/torch.*== /d' *.toml
 ${sed_cmd} -i -e '/install_requires=get_requirement/d' setup.py
 ${sed_cmd} -i -e '/enable_cudnn_sdp/d' vllm/platforms/cuda.py
-${sed_cmd} -i -e '/PYTHON_SUPPORTED_VERSIONS/s/3.9/3.13/g' CMakeLists.txt
+# ${sed_cmd} -i -e '/PYTHON_SUPPORTED_VERSIONS/s/3.9/3.13/g' CMakeLists.txt
 ${sed_cmd} -i -e 's/7.0;7.2;7.5;//g' CMakeLists.txt
 rm requirements*
