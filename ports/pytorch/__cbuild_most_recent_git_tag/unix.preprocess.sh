4cb0428d91abf89fb3cf95770a31364fe03e4b1c
cyy <cyyever@outlook.com>
2025-03-07 20:06:07 +0800

    Fix

--- ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++ ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
@@ -18,6 +18,7 @@ ${sed_cmd} -i -e '/Werror/d' third_party/fbgemm/CMakeLists.txt
 ${sed_cmd} -i -e '/opentelemetry/d' torch/CMakeLists.txt
 ${sed_cmd} -i -e '/-Wno-pass-failed/s/-Wno-pass-failed/-Wno-pass-failed -Wno-deprecated-literal-operator/' CMakeLists.txt
 ${sed_cmd} -i -e '/CXX_STANDARD 17/d' cmake/public/utils.cmake
+${sed_cmd} -i -e '/CONVERT_NON_VECTORIZED_INIT(BFloat16, bfloat16);/s/CONVERT_NON_VECTORIZED_INIT(BFloat16, bfloat16);/CONVERT_NON_VECTORIZED_INIT(BFloat16, bfloat16)/g' aten/src/ATen/cpu/vec/vec256/vec256_bfloat16.h
 
 if [[ "$(uname)" == "FreeBSD" ]]; then
   ${sed_cmd} -i -e 's/_assert/assert_in_pytorch/g' aten/src/ATen/native/sparse/ValidateCompressedIndicesCommon.h
