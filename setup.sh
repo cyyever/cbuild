5c9d79f45aba786cc0f2fd3c12c8104c9c60028b
cyy <cyyever@outlook.com>
2025-03-24 08:00:44 +0800

    Fix code

--- setup.sh
+++ setup.sh
@@ -13,8 +13,8 @@ elif command -v dnf >/dev/null; then
 elif command -v pacman >/dev/null; then
   ${sudo_cmd} pacman -Sy python3 --noconfirm
 elif command -v pkg >/dev/null; then
-  ${sudo_cmd} pkg install -y python312 bash
-  export python3_cmd=python3.12
+  ${sudo_cmd} pkg install -y python311 bash
+  export python3_cmd=python3.11
 elif [[ "$(uname -s)" == "Darwin" ]]; then
   brew_cmd=brew
   if test -f /opt/homebrew/bin/brew; then
