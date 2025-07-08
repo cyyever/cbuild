dc69cd4b56e1962e53c3303872c65a3dd67d6204
cyy <cyyever@outlook.com>
2025-07-08 09:59:42 +0800

    Fix port

--- ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++ ports/pytorch/__cbuild_most_recent_git_tag/unix.preprocess.sh
@@ -32,17 +32,14 @@ ${sed_cmd} -i -e '/CXX_STANDARD 17/d' cmake/public/utils.cmake
 
 if [[ "$(uname)" == "FreeBSD" ]]; then
   ${sed_cmd} -i -e 's/_assert/assert_in_pytorch/g' aten/src/ATen/native/sparse/ValidateCompressedIndicesCommon.h
-  ${sed_cmd} -i -e '/defined(__APPLE__)/s/defined(__APPLE__).*/1/g' third_party/onnx/onnx/checker.cc
 fi
 
 if [[ "${BUILD_CONTEXT_macos:=0}" == "0" ]]; then
   ${sed_cmd} -i -e '/set(TP_BUILD_LIBUV ON CACHE BOOL "" FORCE)/s/ON/OFF/g' cmake/Dependencies.cmake
 fi
-${sed_cmd} -i -e 's/set(FBGEMM_LIBRARY_TYPE "static"/set(FBGEMM_LIBRARY_TYPE "shared"/g' cmake/Dependencies.cmake
-
 
 ${sed_cmd} -i -e '/codecvt_utf8_utf16/d' c10/util/StringUtil.cpp
 ${sed_cmd} -i -e '/erter.to_by/s/return .*/return ss;/g' c10/util/StringUtil.cpp
-${sed_cmd} -i -e 's/fmt::format(shaderSource/fmt::format(fmt::runtime(shaderSource)/g' aten/src/ATen/native/mps/OperationUtils.mm
+# ${sed_cmd} -i -e 's/fmt::format(shaderSource/fmt::format(fmt::runtime(shaderSource)/g' aten/src/ATen/native/mps/OperationUtils.mm
 ${sed_cmd} -i -e '/has_denorm.*;/d' aten/src/ATen/test/half_test.cpp
 ${sed_cmd} -i -e '/has_denorm/d' c10/util/*.h
