b1991a80ec5d94522e48aee6ea54d94ac069a8bd
cyy <cyyever@outlook.com>
2025-08-19 18:41:05 +0800

    Fix vllm

--- ports/vllm/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++ ports/vllm/__cbuild_most_recent_git_tag/unix.preprocess.sh
@@ -3,5 +3,5 @@ ${sed_cmd} -i -e '/torch.*== /d' *.toml
 ${sed_cmd} -i -e '/install_requires=get_requirement/d' setup.py
 ${sed_cmd} -i -e '/librosa/d' setup.py
 ${sed_cmd} -i -e '/mistral_common/d' setup.py
-${sed_cmd} -i -e '/CUDA_SUPPORTED_ARCHS/s/7.0.*8.0;.*"/8.0;"/g' CMakeLists.txt
+${sed_cmd} -i -e '/CUDA_SUPPORTED_ARCHS/s/7.0.*8.0;.*"/8.0;8.6"/g' CMakeLists.txt
 rm -rf requirements*
