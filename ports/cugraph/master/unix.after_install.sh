315e774e380761ee114ca67cb738d4fa6be90568
cyy <cyyever@outlook.com>
2025-09-01 16:05:02 +0800

    Fix code

--- ports/cugraph/master/unix.after_install.sh
+++
@@ -1,9 +0,0 @@
-exit 0
-for _ in $(seq 5); do
-  ${CBUILD_PIP_EXE} uninstall cugraph -y || true
-done
-cd ${SRC_DIR}/python
-rm -rf *.egg-info
-rm -rf build
-${CBUILD_PYTHON_EXE} setup.py build_ext --inplace
-${CBUILD_PYTHON_EXE} setup.py install --user --force
--- ports/cugraph/master/unix.preprocess.sh
+++
@@ -1,3 +0,0 @@
-cd ${SRC_DIR}
-${sed_cmd} -i -e '/CMAKE_CUDA_FLAGS.*OpenMP/d' cpp/CMakeLists.txt
-${sed_cmd} -i -e 's/-DGUNROCK_BUILD_TESTS=OFF/& -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_TOOLKIT_ROOT_DIR} -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}/' cpp/cmake/thirdparty/get_gunrock.cmake
