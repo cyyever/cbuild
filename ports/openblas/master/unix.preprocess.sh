4e37e87e955b1b1a5c1bc636f197fe70cedff874
Yuanyuan Chen <cyyever@outlook.com>
2025-10-20 09:43:17 +0800

    Fix port

--- ports/openblas/master/unix.preprocess.sh
+++ ports/openblas/master/unix.preprocess.sh
@@ -1,1 +1,3 @@
 ${sed_cmd} -i -e 's/if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")/if(${CMAKE_SYSTEM_NAME} MATCHES "Linux" OR ${CMAKE_SYSTEM_NAME} MATCHES "FreeBSD")/' ${SRC_DIR}/utest/CMakeLists.txt
+${sed_cmd} -i -e 's/-dynamiclib//' ${SRC_DIR}/exports/Makefile
+${sed_cmd} -i -e '/CMAKE_Fortran_COMPILER_VERSION.*VERSION_LESS_EQUAL 3/s/if (.*)/if (OFF)/g' ${SRC_DIR}/cmake/system.cmake
