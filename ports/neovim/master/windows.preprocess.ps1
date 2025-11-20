45f805b9600171c6348dee9ba45e4242cf495a25
cyy <cyyever@outlook.com>
2025-11-20 19:47:12 +0800

    More fix

--- ports/neovim/master/windows.preprocess.ps1
+++ ports/neovim/master/windows.preprocess.ps1
@@ -1,2 +1,3 @@
 cd $env:SRC_DIR
 sed_cmd -i -e '/add_compile_options/s/W3/& \/utf-8/' CMakeLists.txt
+sed_cmd -i -e '/set_default_buildtype/s/set_default_buildtype(Debug/set_default_buildtype(Release/' CMakeLists.txt
