eb8f1a2c8083e6bd1c879ea8dac7f005dd39c0c2
cyy <cyyever@outlook.com>
2025-11-21 16:32:40 +0800

    Fix code

--- scripts/package_manager/pip.sh
+++ scripts/package_manager/pip.sh
@@ -1,14 +1,15 @@
-PIP_CMD="${CBUILD_PYTHON_EXE} -m pip -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple"
+PIP_CMD="${CBUILD_PYTHON_EXE} -m pip"
+PIP_INSTALL_CMD="${CBUILD_PYTHON_EXE} -m pip install -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple"
 
 if [[ -n ${DEFAULT_INSTALL_PREFIX+x} ]]; then
   ${CBUILD_PYTHON_EXE} -m ensurepip --default-pip || true
-  ${PIP_CMD} install --upgrade pip
+  ${PIP_INSTALL_CMD} --upgrade pip
   for pkg in ${pip_pkgs}; do
-    ${PIP_CMD} install --upgrade ${pkg}
+    ${PIP_INSTALL_CMD} --upgrade ${pkg}
   done
 else
-  ${PIP_CMD} install --upgrade pip
+  ${PIP_INSTALL_CMD} --upgrade pip
   for pkg in ${pip_pkgs}; do
-    ${PIP_CMD} install --upgrade --user ${pkg}
+    ${PIP_INSTALL_CMD} --upgrade --user ${pkg}
   done
 fi
