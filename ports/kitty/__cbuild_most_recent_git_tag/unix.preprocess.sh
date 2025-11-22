ec3801ef03bff9fa8583d9687fe3f48a66239143
cyy <cyyever@outlook.com>
2025-11-22 08:10:28 +0800

    Apply FreeBSD fixes
    Signed-off-by: cyy <cyyever@outlook.com>

--- ports/kitty/__cbuild_most_recent_git_tag/unix.preprocess.sh
+++
@@ -1,7 +0,0 @@
-if [[ -n ${__SRC_DIR+x} ]]; then
-  cd ${__SRC_DIR}
-  ${sed_cmd} -i -e '/copy_man_pages(ddir)/d' setup.py
-  ${sed_cmd} -i -e '/copy_html_docs(ddir)/d' setup.py
-  # ${sed_cmd} -i -e "/if not os.path.exists('docs/d" setup.py
-  ${sed_cmd} -i -e "/run_tool.*make.*docs/d" setup.py
-fi
